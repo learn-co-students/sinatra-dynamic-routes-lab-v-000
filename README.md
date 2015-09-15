c25268787e8ca4ac906c60eb9eb789b25053ab0c
dfenjves <fenjves.daniel@gmail.com>
2015-09-15 14:04:39 -0400

    created readme

--- README.md
+++ README.md
@@ -5,3 +5,16 @@
 This lab will test students' abilities to create dynamic routes. They should understand and be able to implement routes that add a string or integer to `params` when a request is made. Create a dynamic route that takes in a location ('/post/:location), returning "Hello from sunny #{location}".
 
 Have student repeat dynamic route creation 5-10 times as a drill.
+
+## Overview
+This lab will serve to reinforce your understanding of routes in a Sinatra application. Complete the following tasks in your application controller and get all of the 'learn' tests to pass:
+
++ Create a dynamic route at `get '/reversename/:name'` that accepts a name and renders the name backwards.
+
++ Create a dynamic route at `get '/square/:number'` that accepts a number and returns the square of that number.
+
++ Create a dynamic route at `get '/say/:number/:phrase'` that accepts a number and a phrase and returns that phrase in a string the number of times given.
+
++ Create a dynamic route at `get '/say/:word1/:word2/:word3/:word4/:word5'` that accepts six words and returns a string with the formatted as a sentence.
+
++ Create a dynamic route at `get '/:operation/:number1/:number2'` that accepts an operation (add, subtract, multiply or divide) and performs the operation on the two numbers provided.
---
+++ spec/sinatra_dynamic_routes_lab_spec.rb
@@ -0,0 +1,81 @@
+describe App do
+
+  describe 'GET /reversename/:name' do
+    before do 
+      get '/reversename/victoria'
+    end
+
+    it 'sends a 200 status code' do
+      expect(last_response.status).to eq(200)
+    end
+
+    it 'renders the name backwards' do 
+      expect(last_response.body).to include("airotciv")
+    end
+  end
+
+  describe 'GET /square/:number' do
+    before do 
+      get '/square/4'
+    end
+
+    it 'sends a 200 status code' do
+      expect(last_response.status).to eq(200)
+    end
+
+    it 'renders the square of the number' do 
+      expect(last_response.body).to include("16")
+    end
+  end
+
+  describe 'GET /say/:number/:phrase' do
+    before do 
+      get '/say/3/show%20me%20the%20money'
+    end
+
+    it 'sends a 200 status code' do
+      expect(last_response.status).to eq(200)
+    end
+
+    it 'repeats the phrase n times' do 
+      expect(last_response.body).to include("show me the money\nshow me the money\nshow me the money")
+    end
+  end
+
+  describe 'GET /say/:word1/:word2/:word3/:word4/:word5' do
+    before do 
+      get '/say/stop/rhyming/I/mean/it'
+    end
+
+    it 'sends a 200 status code' do
+      expect(last_response.status).to eq(200)
+    end
+
+    it 'concatenates the words and adds a period' do 
+      expect(last_response.body).to include("stop rhyming I mean it.")
+    end
+  end
+
+    describe 'GET //:operation/:number1/:number2' do
+
+    it 'adds two numbers together' do 
+      get '/add/10/9'
+      expect(last_response.body).to include("19")
+    end
+
+    it 'subtracts the second number from the first' do 
+      get '/subtract/20/9'
+      expect(last_response.body).to include("11")
+    end
+
+    it 'multiplies two numbers together' do 
+      get '/multiply/12/11'
+      expect(last_response.body).to include("132")
+    end
+
+    it 'divides the first number by the second number' do 
+      get '/divide/10/2'
+      expect(last_response.body).to include("5")
+    end
+  end
+end
