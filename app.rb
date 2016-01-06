c229e80f171133148e01a450128b5694a70d7398
Shana L Moore <shana.lavina.moore@gmail.com>
2016-01-06 15:15:12 -0800

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,55 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/reversename/:name' do
+    @name = params[:name]
+    "#{@name.reverse}"
+  end
+
+  get '/square/:number' do
+    @squared = params[:number].to_i * params[:number].to_i
+    "#{@squared}"
+  end
+
+  get '/say/:number/:phrase' do
+    @number = params[:number].to_i
+    @phrase = params[:phrase]
+    final_phrase = ""
+
+    @number.times do
+      final_phrase += "#{@phrase}\n"
+    end
+    final_phrase
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+
+    @word1 = params[:word1]
+    @word2 = params[:word2]
+    @word3 = params[:word3]
+    @word4 = params[:word4]
+    @word5 = params[:word5]
+
+    "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
+  end
+
+  get '/:operation/:number1/:number2'  do
+
+    @num1 = params[:number1].to_i
+    @num2 = params[:number2].to_i
+    @operation = params[:operation]
+
+    case @operation
+    when 'add'
+      result = @num1 + @num2
+    when 'subtract'
+      result = @num1 - @num2
+    when 'multiply'
+      result = @num1 * @num2
+    when 'divide'
+      result = @num1 / @num2
+    end
+      result.to_s
+  end
 end
