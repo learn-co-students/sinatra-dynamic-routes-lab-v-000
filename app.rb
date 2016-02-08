a9e1bbf40aacead8e33e1eccf9350f1c4e04fa82
matthewpaulmcgowan <matthewpaulmcgowan@gmail.com>
2016-02-08 21:39:43 +0000

    completed.

--- app.rb
+++ app.rb
@@ -1,4 +1,50 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    
+  get '/reversename/:name' do 
+    @name = params[:name]
+    @name = @name.reverse
+    "#{@name}"
+  end
+  
+  get '/square/:number' do 
+    @number = params[:number]
+    @number = @number.to_i * @number.to_i
+    "#{@number}"
+  end
+  
+  get '/say/:number/:phrase' do 
+    @number = params[:number].to_i
+    @phrase = params[:phrase]
+    "#{@phrase}\n" * @number
+  end
+  
+  get "/say/:word1/:word2/:word3/:word4/:word5" do
+    @word1 = params[:word1]
+    @word2 = params[:word2]
+    @word3 = params[:word3]
+    @word4 = params[:word4]
+    @word5 = params[:word5]
+    "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
+  end
+  
+  get "/:operation/:number1/:number2" do 
+    @operation = params[:operation]
+    @num1 = params[:number1].to_i
+    @num2 = params[:number2].to_i
+    if @operation == "add"
+      @result = @num1+@num2
+      "#{@result}"
+    elsif @operation == "subtract"
+      @result = @num1-@num2
+      "#{@result}"
+    elsif @operation == "multiply"
+      @result = @num1*@num2
+      "#{@result}"
+    elsif @operation == "divide"
+      @result = @num1/@num2
+      "#{@result}"
+    end
+  end
 end
