3e5d95cd9814acca6f80ab6992182b3c81e4b3d7
glee38 <glee3388@gmail.com>
2016-03-28 22:36:17 -0400

    Done.

--- app.rb
+++ app.rb
@@ -4,8 +4,11 @@ require 'pry'
 class App < Sinatra::Base
 
  get '/reversename/:name' do
-   @user_name = params[:name]
-   @user_name.reverse
+   # @user_name = params[:name]
+   # @user_name.reverse
+
+   #refactored to:
+   params[:name].reverse
  end
 
  get '/square/:number' do
@@ -21,13 +24,16 @@ class App < Sinatra::Base
  end
 
  get '/say/:word1/:word2/:word3/:word4/:word5' do
-  @w1 = params[:word1]
-  @w2 = params[:word2]
-  @w3 = params[:word3]
-  @w4 = params[:word4]
-  @w5 = params[:word5]
+  # @w1 = params[:word1]
+  # @w2 = params[:word2]
+  # @w3 = params[:word3]
+  # @w4 = params[:word4]
+  # @w5 = params[:word5]
 
-  "#{@w1} #{@w2} #{@w3} #{@w4} #{@w5}."
+  # "#{@w1} #{@w2} #{@w3} #{@w4} #{@w5}."
+
+  # refactored to:
+  "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
  end
 
  get '/:operation/:number1/:number2' do
