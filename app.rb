f4397070cd44e25e13459ff83e438ee085a5b7ea
katherinethai <kthai1618@gmail.com>
2016-04-15 00:40:59 -0400

    Done.

--- app.rb
+++ app.rb
@@ -16,7 +16,11 @@ class App < Sinatra::Base
     @num = params[:number].to_i
     @phrase = params[:phrase]
 
-    "#{@phrase}\n#{@phrase}\n#{@phrase}"
+    string = ""
+    @num.times do 
+      string += "#{@phrase}\n"
+    end
+    string
   end
 
   get "/say/:word1/:word2/:word3/:word4/:word5" do
