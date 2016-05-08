de0ad94f72f0e8f450339bd4748633dc7dfe8d5e
thebrianemory <the@brianemory.com>
2016-05-08 14:02:35 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,48 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/reversename/:name' do
+    @name = params[:name]
+    "#{@name.reverse}"
+  end
+
+  get '/square/:number' do
+    @number = params[:number].to_i ** 2
+    "#{@number}"
+  end
+
+  get '/say/:number/:phrase' do
+    @number = params[:number].to_i
+    @phrase = params[:phrase]
+    "#{@phrase}\n" * @number
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    @word1 = params[:word1]
+    @word2 = params[:word2]
+    @word3 = params[:word3]
+    @word4 = params[:word4]
+    @word5 = params[:word5]
+    "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
+  end
+
+  get '/:operation/:number1/:number2' do
+    @operator = params[:operation]
+    @number1 = params[:number1].to_i
+    @number2 = params[:number2].to_i
+    if @operator == "add"
+      answer = @number1 + @number2
+      "#{answer}"
+    elsif @operator == "subtract"
+      answer = @number1 - @number2
+      "#{answer}"
+    elsif @operator == "multiply"
+      answer = @number1 * @number2
+      "#{answer}"
+    else @operator == "divide"
+      answer = @number1 / @number2
+      "#{answer}"
+    end
+  end
 end
