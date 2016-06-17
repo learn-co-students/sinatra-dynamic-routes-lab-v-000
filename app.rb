80b1a655969af4f16e1a1e23255cc5195f29bf35
Christian Whitesides <whitesidescr@gmail.com>
2016-06-17 16:57:08 -0600

    Done.

--- app.rb
+++ app.rb
@@ -3,41 +3,59 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/reversename/:name' do
-    @name = params[:name]
-    "#{@name.reverse}"
+    # My solution:
+    # name = params[:name]
+    # "#{name.reverse}"
+
+    params[:name].reverse
   end
 
   get '/square/:number' do
-    @number_to_square = params[:number].to_i
-    "#{@number_to_square ** 2}"
+    # My solution:
+    # number = params[:number].to_i
+    # "#{number ** 2}"
+
+    num = params[:number]
+    (num.to_i**2).to_s
   end
 
   get '/say/:number/:phrase' do
-    @number = params[:number].to_i
-    @phrase = params[:phrase]
+    # My solution:
+    # number = params[:number].to_i
+    # phrase = params[:phrase]
+    # "#{phrase}\n" * number
 
-    "#{@phrase}\n" * @number
+    final_string =  ""
+    (params[:number].to_i).times do
+      final_string += "#{params[:phrase]}\n"
+    end
+    final_string
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
-    @word1, @word2, @word3, @word4, @word5 = params[:word1], params[:word2], params[:word3], params[:word4], params[:word5]
+    # My solution:
+    # word1, word2, word3, word4, word5 = params[:word1], params[:word2], params[:word3], params[:word4], params[:word5]
+    # "#{word1} #{word2} #{word3} #{word4} #{word5}."
 
-    "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
+    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end
 
   get '/:operation/:number1/:number2' do
-    @operation = params[:operation]
-    @num1 = params[:number1].to_i
-    @num2 = params[:number2].to_i
+    operation = params[:operation]
+    number1 = params[:number1].to_i
+    number2 = params[:number2].to_i
 
-    if @operation == "add"
-      "#{@num1 + @num2}"
-    elsif @operation == "subtract"
-      "#{@num1 - @num2}"
-    elsif @operation == "multiply"
-      "#{@num1 * @num2}"
+    if operation == "add"
+      answer = number1 + number2
+    elsif operation == "subtract"
+      answer = number1  number2
+    elsif operation == "multiply"
+      answer = number1 * number2
+    elsif operation == "divide"
+      answer = number1 / number2
     else
-      "#{@num1 / @num2}"
+      "Unable to perform this operation"
     end
+    answer.to_s
   end
 end
