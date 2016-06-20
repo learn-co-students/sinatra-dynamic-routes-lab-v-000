ea409f42d2189de6902f0d58a58dbfaae02ee87e
MIKE-RANDELL <michael.james.randell@gmail.com>
2016-06-20 14:37:51 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/reversename/:name' do
+    @revname = params[:name]
+    @revname.reverse
+  end
+
+  get '/square/:number' do
+    @numb = params[:number].to_i
+    @square = @numb * @numb
+    @square.to_s
+  end
 end
