8cd854b32e8cdaa400c1a2b41000cc30ca94f2bd
Ivan Pilot <ivanpilot@gmail.com>
2016-07-07 20:27:22 +0000

    Done.

--- app.rb
+++ app.rb
@@ -12,20 +12,12 @@ class App < Sinatra::Base
   end
 
   get '/say/:number/:phrase' do
-    # binding.pry
     n = params[:number].to_i
-    "#{n.to_s}"
-    # sentence = "#{params[:phrase]}"
-    # n.times{sentence}
-    "ivan"
-    # counter = 1
-    # while counter < n + 1
-    #   # sentence
-    #   "Ivan"
-    #   # "params[:phrase]"
-    #   counter += 1
-    # end
-    # binding.pry
+    result = ""
+    n.times do
+      result += "#{params[:phrase]}\n"
+    end
+    result
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
@@ -34,7 +26,7 @@ class App < Sinatra::Base
 
   get '/:operation/:number1/:number2' do
     operation = params[:operation]
-    case 'operation'
+    result = case operation
     when "add" then "#{params[:number1].to_i + params[:number2].to_i}"
     when "subtract" then "#{params[:number1].to_i - params[:number2].to_i}"
     when "multiply" then "#{params[:number1].to_i * params[:number2].to_i}"
