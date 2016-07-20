d2a50ccd4475b79d366bdd228edd93689347d029
Jason Casares <jasonc@csdrafting.com>
2016-07-20 15:30:54 +0000

    Done.

--- app.rb
+++ app.rb
@@ -14,12 +14,12 @@ class App < Sinatra::Base
     "#{numbers.to_s}"
   end
 
-  get '/say/:number/phrase' do
-    @number = params[:number]
-    @phrase = params[:phrase]
-    @number.times do
-      "#{@phrase}"
+  get '/say/:number/:phrase' do
+    final_string = ""
+    (params[:number].to_i).times do
+      final_string += "#{params[:phrase]}\n"
     end
+    final_string
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
@@ -32,12 +32,23 @@ class App < Sinatra::Base
     "#{words}"
   end
 
-  #get '/:operation/:number1/:number2' do
-    #@operation = params[:operation]
-    #@number1 = params[:number1]
-    #@number2 = params[:number2]
-    #numbers = @number1 @operation @number2
-    #{}"#{numbers}"
-  #end
+  get '/:operation/:number1/:number2' do
+     operation = params[:operation]
+     number1 = params[:number1].to_i
+     number2 = params[:number2].to_i
+     if operation == "add"
+       answer = number1 + number2
+     elsif operation == "subtract"
+       answer = number1 - number2
+     elsif operation == "multiply"
+       answer = number1 * number2
+     elsif operation == "divide"
+       answer = number1 / number2
+     else
+       answer = "Unable to perform this operation"
+     end
+     answer.to_s
+   end
+
 
 end
