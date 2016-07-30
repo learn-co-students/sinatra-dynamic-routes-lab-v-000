addf71eaa898e23314335c77f0562360689d432a
Jonathan Stone <jon_p_stone@yahoo.com>
2016-07-30 08:13:14 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,26 +1,45 @@
 require_relative 'config/environment'
+require "pry"
 
 class App < Sinatra::Base
 
   get '/reversename/:name' do
-    @reversed_name = param[:name].reverse!
+    params[:name].reverse
   end
 
   get '/square/:number' do
-    @squared_number = param[:number] * param[:number]
+    num = params[:number]
+    (num.to_i**2).to_s
   end
 
   get '/say/:number/:phrase' do
-    phrase = param[:phrase]
-    param[:number].times {phrase}
+    final_string = ""
+    (params[:number].to_i).times do
+      final_string += "#{params[:phrase]}\n"
+    end
+    final_string
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
-    @sentence = "#{param[:word1]} " + "#{param[:word2]} " + "#{param[:word3]} " + "#{param[:word4]} " + "#{param[:word5]}"
+    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end
 
   get '/:operation/:number1/:number2' do
-    @result = p
+    operation = params[:operation]
+    number1 = params[:number1].to_i
+    number2 = params[:number2].to_i
+    if operation == "add"
+      answer = number1 + number2
+    elsif operation == "subtract"
+      answer = number1 - number2
+    elsif operation == "multiply"
+      answer = number1 * number2
+    elsif operation == "divide"
+      answer = number1 / number2
+    else
+      answer = "Unable to perform this operation"
+    end
+    answer.to_s
   end
 
 end
