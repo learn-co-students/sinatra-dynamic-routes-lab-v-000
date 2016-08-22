61fb326cab701659c307411218a599c500dbbd45
ThisIsTyDell <milleb94@gmail.com>
2016-08-22 13:00:53 -0500

    add more dynamic routes to display multiple phrases, and also another dynamic route to display a sentence

--- app.rb
+++ app.rb
@@ -16,4 +16,15 @@ class App < Sinatra::Base
     "#{squared}"
   end
 
+  get '/say/:number/:phrase' do
+    multi_phrase = ""
+    (params[:number].to_i).times { multi_phrase += "#{params[:phrase]}\n" }
+    multi_phrase
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    sentence = "#{params[:captures].join(" ")}" + "."
+    sentence
+  end
+
 end
