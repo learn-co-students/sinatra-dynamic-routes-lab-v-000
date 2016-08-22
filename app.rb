ed601589bd3bec3abfa23bb923968b2e7822baab
ThisIsTyDell <milleb94@gmail.com>
2016-08-22 13:14:07 -0500

    add dynamic route to /:operation/:number1/:number2 to do simple math

--- app.rb
+++ app.rb
@@ -27,4 +27,23 @@ class App < Sinatra::Base
     sentence
   end
 
+  get '/:operation/:number1/:number2' do
+    operation = params[:operation]
+    num1 = params[:number1].to_i
+    num2 = params[:number2].to_i
+
+    case operation
+      when "add"
+        "#{num1 + num2}"
+      when "subtract"
+        "#{num1 - num2}"
+      when "multiply"
+        "#{num1 * num2}"
+      when "divide"
+        "#{num1 / num2}"
+      else
+        "Whoops, try again. Enter / add, subtract, multiply, or divide. / number 1 / number 2."
+      end
+  end
+
 end
