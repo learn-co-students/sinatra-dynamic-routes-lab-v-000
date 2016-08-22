01350846fb30809c6815110de35b69b421748153
ThisIsTyDell <milleb94@gmail.com>
2016-08-22 11:45:45 -0500

    Add routes for /reversename and /square to test out in browser

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello World"
+  end
+
+  get '/reversename' do
+    "Reverse that joint"
+  end
+
+  get '/square' do
+    "Square that joint"
+  end
+  
 end
