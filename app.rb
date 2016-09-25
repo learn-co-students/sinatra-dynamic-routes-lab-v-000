b98c3ed1ebfd52bdddda0ce123fce0c2d2aec607
Samuel Lair <lair001@gmail.com>
2016-09-25 02:07:07 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,29 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "<h1>Why are you here?</h1>"
+  end
+
+  get '/reversename/:name' do
+    erb :reversename
+  end
+
+  get '/square/:number' do
+    erb :square
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    erb :say5
+  end
+
+  get '/say/:number/:phrase' do
+    erb :say
+  end
+
+  get '/:operation/:number1/:number2' do
+    erb :operation    
+  end
+
 end
