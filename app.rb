e7814614c23bdb6bdda4f05bcac09f3a55a6bd77
Jon Fougner <jon.fougner@gmail.com>
2016-11-28 18:43:46 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,45 @@
+require 'pry'
+
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/reversename/:name' do
+    params[:name].reverse
+  end
+
+  get '/square/:number' do
+    # binding.pry
+    @square = params[:number].to_i ** 2
+    @square.to_s
+  end
+
+  get '/say/:number/:phrase' do
+    # binding.pry
+    params[:number].to_i.times.map {
+      "#{params[:phrase]}\n"
+    }
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
+  end
+
+  get '/:operation/:number1/:number2' do
+    # binding.pry
+    output = nil
+    if params[:operation] == "add"
+      # binding.pry
+      output = params[:number1].to_i + params[:number2].to_i
+    elsif params[:operation] == "subtract"
+      output = params[:number1].to_i - params[:number2].to_i
+    elsif params[:operation] == "multiply"
+      output = params[:number1].to_i * params[:number2].to_i
+    elsif params[:operation] == "divide"
+      output = params[:number1].to_i / params[:number2].to_i      
+    end
+    # binding.pry
+    output.to_s
+  end
+
 end
