5aa9d25467a8be720be84100b880424388e2f529
mjdele <mike.delehant@gmail.com>
2016-12-15 16:03:33 -0500

    Done.

--- app.rb
+++ app.rb
@@ -3,8 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get "/reversename/:name" do
-    @user_name = params[:name].reverse
-    @user_name
+    params[:name].reverse
   end
 
   get "/square/:number" do
@@ -19,23 +18,24 @@ class App < Sinatra::Base
   end
 
   get "/say/:word1/:word2/:word3/:word4/:word5" do
-    @phrase = "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
-    @phrase
+    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end
 
   get "/:operation/:number1/:number2" do
     @operation = params[:operation]
+    @number1 = params[:number1].to_i
+    @number2 = params[:number2].to_i
     if @operation == "add"
-      @solution = params[:number1].to_i + params[:number2].to_i
+      @solution = @number1 + @number2
       @solution.to_s
     elsif @operation == "subtract"
-      @solution = params[:number1].to_i - params[:number2].to_i
+      @solution = @number1 - @number2
       @solution.to_s
     elsif @operation == "multiply"
-      @solution = params[:number1].to_i * params[:number2].to_i
+      @solution = @number1 * @number2
       @solution.to_s
     elsif @operation == "divide"
-      @solution = params[:number1].to_i / params[:number2].to_i
+      @solution = @number1 / @number2
       @solution.to_s
     end
   end
