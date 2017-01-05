6f4bf1885a1466501354ab9c5fd569437b38d94e
Mary Umoh <umohm12@gmail.com>
2017-01-05 16:24:45 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,36 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/reversename/:name' do
+   @user_name = params[:name]
+  "#{@user_name}".reverse.split
+ end
+
+ get '/square/:number' do
+    @number = params[:number]
+   "#{@number ** 2}.to_i" #"These words are strings #{ Ruby code to be evaluated and then converted to string}"
+ end
+
+ get '/say/:number/:phrase' do
+     @new_phrase = ""
+     params[:number].to_i.times do |i|
+     @new_phrase << params[:phrase] + "\n"
+     @new_phrase
+ end
+     
+
+ get '/say/:word1/:word2/:word3/:word4/:word5' do
+   @concatenate = params[:word1] + params[:word2] + params[:word3] + params[:word4] + params[:word5]
+   #"#{@concatenate.concat}." creates a loop
+ end
+
+ get "/:operation/:number1/:number2" do
+  @sum = params[:number1] + [:number2]
+  "#{@sum}"
+  #params[:number2] - [:number1]
+  #params[:number1] * [:number2]
+  #params[:number1] / [:number2]
+ end
+end
 end
