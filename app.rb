7fdb04ee4a082b08474185ff45adc25c51298c35
Mary Umoh <umohm12@gmail.com>
2017-01-07 00:28:15 +0000

    Done.

--- app.rb
+++ app.rb
@@ -14,24 +14,29 @@ class App < Sinatra::Base
 
   get '/say/:number/:phrase' do
     @new_phrase = " "
-    params[:number].to_i.times do |i|
+    (params[:number].to_i).times do |i|
     @new_phrase << params[:phrase] + "\n"
+  end
     @new_phrase
   end
-end
-
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
-    @new_str = params[:word1].to_s + params[:word2].to_s + params[:word3].to_s + params[:word4].to_s + params[:word5]to._s
-    "#{new_str}.concat.join('+')"
+    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end
 
 
   get "/:operation/:number1/:number2" do
-    @sum = params[:number1] + [:number2]
-   "#{@sum}"
-   #params[:number2] - [:number1]
-   #params[:number1] * [:number2]
-   #params[:number1] / [:number2]
-  end
+    operation = params[operation]
+    number1 = params[:number1].to_i
+    number2 = params[:number2].to_i
+    if operation == "add"
+      answer = number1 + number2
+    elsif operation == 'divide'
+      answer = num1 / num2
+    elsif operation == 'subtract'
+      answer = num1 - num2
+    else operation == 'multiply'
+      answer = num1 * num2
+   end
+ end
 end
