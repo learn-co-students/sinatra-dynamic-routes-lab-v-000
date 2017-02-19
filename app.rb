4e9acd5f0fa237f17710e1bd918315e1d0a07e6d
Michael Djurdjevic <mdjurdj1@binghamton.edu>
2017-02-19 16:46:11 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,50 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/reversename/:name' do
+    @reversed = params[:name].reverse
+  end
+
+  get '/square/:number' do
+    @squared = params[:number].to_i**2
+    @squared.to_s
+  end
+
+  get '/say/:number/:phrase' do
+    @number = params[:number].to_i
+    @phrase = params[:phrase]
+    output = ""
+      @number.times do
+       output += "#{@phrase}\n"
+    end
+    output
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    @word1 = params[:word1]
+    @word2 = params[:word2]
+    @word3 = params[:word3]
+    @word4 = params[:word4]
+    @word5 = params[:word5]
+    formatted = "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
+  end
+
+  get '/:operation/:number1/:number2' do
+    @num1 = params[:number1].to_i
+    @num2 = params[:number2].to_i
+    @operation = params[:operation]
+    multiplied = ""
+    if @operation == "add"
+      multiplied += @num1 + @num2
+    elsif @operation == "multiply"
+      multiplied += @num1 * @num2
+    elsif @operation == "subtract"
+      multiplied += @num1 - @num2
+    elsif @operation == "divide"
+      multiplied += @num1 / @num2
+    end
+    multiplied
+  end
+
 end
