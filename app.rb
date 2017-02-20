a9a5bc88ff018b7aab6c6b04c4bcc636fa66362a
John Pfingst <jpfingst@outlook.com>
2017-02-20 22:52:21 +0000

    more Sinatra dynmaic route work

--- app.rb
+++ app.rb
@@ -1,4 +1,45 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get "/reversename/:name" do
+    @name = params[:name]
+    "Hello #{@name.reverse}!"
+  end
+
+  get "/square/:number" do
+    @number = params[:number].to_i
+    "Sqaure #{@number*@number}!"
+  end
+
+  get "/say/:number/:phrase" do
+    @number = params[:number].to_i
+    @phrase = params[:phrase] + "\n"
+    @phrase * @number
+  end
+
+  get "/say/:word1/:word2/:word3/:word4/:word5" do
+    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
+  end
+
+
+  get "/:operation/:number1/:number2" do
+    @operation = params[:operation]
+    @number1   = params[:number1].to_i
+    @number2   = params[:number2].to_i
+
+    case @operation
+    when 'add'
+      "#{@number1+@number2}"
+    when 'subtract'
+      "#{@number1-@number2}"
+    when 'multiply'
+      "#{@number1*@number2}"
+    when 'divide'
+      "#{@number1/@number2}"
+    else
+      "Invalid operator"
+    end
+  end
+
 end
