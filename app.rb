75954aa182ebdb1f3377a545e99d9971c28d60c9
Benny Hudson Hung <wakenightryu@gmail.com>
2017-03-21 00:18:06 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,55 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get "/reversename/:name" do
+    @name = params[:name]
+    "#{@name.reverse}"
+  end
+
+  get "/square/:number" do
+    @number = params[:number]
+    "#{@number.to_i * @number.to_i}"
+  end
+
+  get "/say/:word1/:word2/:word3/:word4/:word5" do
+    @word1 = params[:word1]
+    @word2 = params[:word2]
+    @word3 = params[:word3]
+    @word4 = params[:word4]
+    @word5 = params[:word5]
+    "#{@word1} " + "#{@word2} " + "#{@word3} " + "#{@word4} " + "#{@word5}" + "."
+  end
+
+  get "/say/:number/:phrase" do
+    @number = params[:number]
+    @phrase = params[:phrase]
+    array = []
+    @number.to_i.times do
+      array << @phrase
+    end
+    array.join("\n")
+  end
+
+  get "/:operation/:number1/:number2" do
+    @number1 = params[:number1]
+    @number2 = params[:number2]
+    @operation = params[:operation]
+    if @operation == "add"
+      "#{@number1.to_i + @number2.to_i}"
+    elsif @operation == "subtract"
+      "#{@number1.to_i - @number2.to_i}"
+    elsif @operation == "multiply"
+      "#{@number1.to_i * @number2.to_i}"
+    elsif @operation == "divide"
+      "#{@number1.to_i / @number2.to_i}"
+    end
+  end
+
+
+
+
+
+
+
 end
