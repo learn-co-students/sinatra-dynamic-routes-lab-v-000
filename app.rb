22427d8592b70f23e6e36ca5acafb8fbf9adaf8a
Efrain Perez Jr <efrainperezjr@live.com>
2017-03-25 15:25:19 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,44 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get "/" do
+    "Hey!!!"
+  end
+
+  get "/reversename/:name" do
+    "#{params[:name].reverse}"
+  end
+
+  get "/square/:number" do
+    "#{params[:number].to_i ** 2}"
+  end
+
+  get "/say/:number/:phrase" do
+    @number = params[:number].to_i
+    @phrase = params[:phrase]
+    @number.times.collect {@phrase+"\n"}
+  end
+
+  get "/say/:word1/:word2/:word3/:word4/:word5" do
+    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
+  end
+
+  get "/:operation/:number1/:number2" do
+    @num1 = params[:number1].to_i
+    @num2 = params[:number2].to_i
+    case params[:operation]
+      when "add"
+        "#{@num1 + @num2}"
+      when "subtract"
+        "#{@num1 - @num2}"
+      when "multiply"
+        "#{@num1 * @num2}"
+      when "divide"
+        "#{@num1 / @num2}"
+      else
+        "Operation doesn't exist"
+    end
+  end
+
 end
