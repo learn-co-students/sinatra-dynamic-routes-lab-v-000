e25ca3bbcf244593df58b8b777e982cddbb21e7d
Sergio Mendoza <sergiomendoza@Sergios-MacBook-Pro.local>
2017-04-28 23:54:42 -0700

    Done.

--- app.rb
+++ app.rb
@@ -11,44 +11,44 @@ class App < Sinatra::Base
   end
 
   get "/say/:word1/:word2/:word3/:word4/:word5" do
-    @word1 = params[:word1]
-    @word2 = params[:word2]
-    @word3 = params[:word3]
-    @word4 = params[:word4]
-    @word5 = params[:word5]
+    word1 = params[:word1]
+    word2 = params[:word2]
+    word3 = params[:word3]
+    word4 = params[:word4]
+    word5 = params[:word5]
 
-    @sentence = "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
-    erb :say
+    sentence = "#{word1} #{word2} #{word3} #{word4} #{word5}."
+    sentence
   end
 
   get "/say/:number/:phrase" do
-    @number = params[:number].to_i
-    @phrase = params[:phrase]
-    @sentence = ""
+    number = params[:number].to_i
+    phrase = params[:phrase]
+    sentence = ""
 
-    @number.times do
-      @sentence += @phrase + "\n"
+    number.times do
+      sentence += phrase + "\n"
     end
 
-    erb :say
+    sentence
   end
 
   get '/:operation/:number1/:number2' do
-    @operation = params[:operation]
-    @number1 = params[:number1].to_i
-    @number2 = params[:number2].to_i
+    operation = params[:operation]
+    number1 = params[:number1].to_i
+    number2 = params[:number2].to_i
 
-    case @operation
+    case operation
     when "add"
-      @answer = @number1 + @number2
+      answer = number1 + number2
     when "subtract"
-      @answer = @number1 - @number2
+      answer = number1 - number2
     when "multiply"
-      @answer = @number1 * @number2
+      answer = number1 * number2
     when "divide"
-      @answer = @number1 / @number2
+      answer = number1 / number2
     end
 
-    erb :operation
+    answer.to_s
   end
 end
