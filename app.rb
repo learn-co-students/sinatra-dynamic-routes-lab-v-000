aec9e6b4552d97ac5bf48e0e50fea5eec31a58a9
Sophia Varella <sophiavarella@gmail.com>
2017-05-02 21:06:28 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,48 @@
-require_relative 'config/environment'
+require_relative './config/environment'
 
 class App < Sinatra::Base
+
+  get '/reversename/:name' do
+    @reversed_name = params[:name].reverse
+    "#{@reversed_name}"
+    # "#{params[:name].split("").reverse.join}"
+  end
+
+  get '/square/:number' do
+    @number = params[:number].to_i
+    "#{@number * @number}"
+  end
+
+  get '/say/:number/:phrase' do
+    @number = params[:number].to_i
+    @phrase = params[:phrase]
+    @say = ""
+    
+    @number.times do 
+      @say += "#{@phrase}\n"
+    end
+
+    "#{@say}"
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
+  end
+
+  get '/:operation/:number1/:number2' do
+    @operation = params[:operation]
+    @num1 = params[:number1].to_i
+    @num2 = params[:number2].to_i
+
+    case @operation
+    when "add"
+      "#{@num1 + @num2}"
+    when "subtract"
+      "#{@num1 - @num2}"
+    when "multiply"
+      "#{@num1 * @num2}"
+    when "divide"
+      "#{@num1 / @num2}"
+    end
+  end
 end
