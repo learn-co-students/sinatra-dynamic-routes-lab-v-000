0270830bd5d4afe5dcc1b0761e567b5352072275
Dave Schlauderaff <dschlaud@gmail.com>
2017-06-09 19:11:55 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,31 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Dynamic routes lab"
+  end
+
+  get '/reversename/:name' do
+    @name = params[:name].reverse
+    "#{@name}"
+  end
+
+  get '/square/:number' do
+    @number = params[:number].to_i
+    "#{@number * @number}"
+  end
+
+  get '/say/:number/:phrase' do
+    @number = params[:number].to_i
+    @phrase = params[:phrase]
+    # binding.pry
+    @number.times do
+      @phrase = @phrase + "\n#{@phrase}"
+    end
+    "#{@phrase}"
+  end
+
+
+
 end
