a258faacce3a1d6c5541d3a338c4f183883f6716
Will-Sauer <will@heueso.com>
2017-06-15 13:17:48 -0700

    finished

--- app.rb
+++ app.rb
@@ -1,4 +1,38 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+get "/reversename/:name" do
+    erb :reversename
+end
+
+
+get "/square/:number" do
+    erb :square
+end
+
+get "/say/:number/:phrase" do
+    erb :say_phrase
+end
+
+get "/say/:word1/:word2/:word3/:word4/:word5" do
+    erb :say_words
+end
+
+
+get "/:operation/:number1/:number2" do
+
+    if params[:operation] == "add"
+       erb :add    
+    elsif params[:operation] == "subtract"
+       erb :subtract
+    elsif params[:operation] == "multiply"
+       erb :multiply
+    elsif params[:operation] == "divide"
+       erb :divide
+    end
+end
+
+
+
 end
