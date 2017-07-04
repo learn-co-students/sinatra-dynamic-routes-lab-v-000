4d637071179dad92a025594db58e9a9d62498b67
Scott Wakeman <swakeman@ca.rr.com>
2017-07-04 18:06:38 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,46 +2,64 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
-  get '/reversename/:name'
+  get '/reversename/:name' do
   #accepts a name and renders the name backwards.
-  @name = params[:name]
-  reversename = @name.reverse
-  "#{reversename}"
+    @name = params[:name]
+    reversename = @name.reverse
+      "#{reversename}"
   end
 
-  get '/square/:number'
+  get '/square/:number' do
   #accepts a number and returns the square of that number.
   #Note: Remember that values in params always come in as strings, and your return value for the route should also be a string (use .to_i and .to_s).
-  @number = params[:number]
-  squared_number = @number.to_i * @number.to_i
-  "#{squared_number}"
+    @number = params[:number]
+    squared_number = @number.to_i * @number.to_i
+      "#{squared_number}"
   end
 
-  get '/say/:number/:phrase'
+  get '/say/:number/:phrase' do
   #accepts a number and a phrase and returns that phrase in a string the number of times given.
-  @number = params[:number]
-  @phrase = parmas[:phrase]
-  "#{phrase} * #{number}"
+    @number = params[:number]
+    @phrase = parmas[:phrase]
+      "#{phrase} * #{number.to_i}"
   end
 
-  get '/say/:word1/:word2/:word3/:word4/:word5'
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
   #accepts five words and returns a string with them formatted as a sentence.
-  @word1 = params[:word1]
-  @word2 = params[:word2]
-  @word3 = params[:word3]
-  @word4 = params[:word4]
-  @word5 = params[:word5]
-  sentence = @word1 + @word2 + @word3 + @word4 + @word5
-  "#{sentenc}.join(" ")"
+    @word1 = params[:word1]
+    @word2 = params[:word2]
+    @word3 = params[:word3]
+    @word4 = params[:word4]
+    @word5 = params[:word5]
+      "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
   end
 
-  get '/:operation/:number1/:number2'
+  get '/:operation/:number1/:number2' do
   #accepts an operation (add, subtract, multiply or divide) and performs the operation on the two numbers provided.
-  @number1 = params[:number1]
-  @number2 = params[:number2]
-  @add = params[:number1] + [:number2]
-  @subtract = [:number1] - [:number2]
-  @multiply = [:number1] * [:number2]
-  @divide = [:number1] / [:number2]
+    @number1 = params[:number1]
+    @number2 = params[:number2]
+      if @operation == "add"
+        addition = @number1.to_i + @number2.to_i
+        "#{addition}"
+        #"{([:number1] + [:number2])}"
+
+      elsif @operation == "subtract"
+        subtration = @number1.to_i - @number2.to_i
+        "#{subtration}"
+        #{}"{([:number1] - [:number2])}"
+
+      elsif @operation == "multiply"
+        multiplication = @number1.to_i * @number2.to_i
+        "#{multiplication}"
+        #{}"{([:number1] * [:number2])}"
+
+      elsif @operation == "divide"
+        division = @number1 / @number2
+        "#{division}"
+        #"{([:number1] / [:number2])}"
+
+      #else
+        #{}"I did not recognize the operation"
+      end
   end
 end
