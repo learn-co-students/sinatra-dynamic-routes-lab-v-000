930813d106f6fa59cbf06ef4187fad0c95aebfb6
Scott Wakeman <swakeman@ca.rr.com>
2017-07-05 02:09:59 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,9 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello World"
+  end
   # Write your code here!
   get '/reversename/:name' do
   #accepts a name and renders the name backwards.
@@ -19,9 +22,9 @@ class App < Sinatra::Base
 
   get '/say/:number/:phrase' do
   #accepts a number and a phrase and returns that phrase in a string the number of times given.
-    @number = params[:number]
-    @phrase = parmas[:phrase]
-      "#{phrase} * #{number.to_i}"
+    @number = params[:number].to_i
+    @phrase = params[:phrase]
+    erb :say_phrase
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
@@ -36,28 +39,25 @@ class App < Sinatra::Base
 
   get '/:operation/:number1/:number2' do
   #accepts an operation (add, subtract, multiply or divide) and performs the operation on the two numbers provided.
-    @number1 = params[:number1]
-    @number2 = params[:number2]
+    @number1 = params[:number1].to_i
+    @number2 = params[:number2].to_i
+    @operation = params[:operation]
       if @operation == "add"
-        addition = @number1.to_i + @number2.to_i
+        addition = @number1 + @number2
           "#{addition}"
-        #"{([:number1] + [:number2])}"
 
       elsif @operation == "subtract"
-        subtraction = @number1.to_i - @number2.to_i
+        subtraction = @number1 - @number2
           "#{subtraction}"
-        #{}"{([:number1] - [:number2])}"
 
       elsif @operation == "multiply"
-        multiplication = @number1.to_i * @number2.to_i
+        multiplication = @number1 * @number2
           "#{multiplication}"
-        #{}"{([:number1] * [:number2])}"
 
       else
          @operation == "divide"
           division = @number1 / @number2
             "#{division}"
-        #"{([:number1] / [:number2])}"
       end
   end
 end
