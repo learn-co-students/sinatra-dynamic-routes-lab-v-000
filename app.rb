376abdfd3dca24d95237b1e435e074213b2629fc
Seb Gertz <sebgertz@gmail.com>
2017-07-30 22:32:47 +0000

    Done.

--- app.rb
+++ app.rb
@@ -32,7 +32,7 @@ class App < Sinatra::Base
     @word4= params[:word4]
     @word5= params[:word5]
 
-    sentence = [@word1, @word2, @word3, @word4, @word5].join(" ") + "."
+    [@word1, @word2, @word3, @word4, @word5].join(" ") + "."
   end
 
   get '/:operation/:number1/:number2' do
