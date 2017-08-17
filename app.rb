8b9b4dfb60ebd1cfe7c265cf17700bc70e6b42fb
Nicholas D'Amico <nickalan82@icloud.com>
2017-08-17 23:40:13 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,29 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+  get '/reversename/:name' do
+    erb :reversename
+  end
 
+  get '/square/:number' do
+    @num = params[:number].to_i
+    erb :square
+  end
+
+  get '/say/:number/:phrase' do
+    erb :say_phrase
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    @msg = ''
+    params.each.map { |k,v| @msg += "#{v} "}
+    "#{@msg.strip}."
+  end
+
+  get '/:operation/:number1/:number2' do
+    
+    @num1 = params[:number1].to_i
+    @num2 = params[:number2].to_i
+    erb :operation
+  end
 end
