6fb0d9eea4791948b0a4ea033aaf28e199c03d96
Dalia Sawaya <sawaya.dalia@gmail.com>
2017-09-08 18:25:12 +0000

    Done.

--- app.rb
+++ app.rb
@@ -14,18 +14,34 @@ class App < Sinatra::Base
     integer.to_s
   end
 
-  get 'say/:number/:phrase' do
-    num = params[:number]
+  get '/say/:number/:phrase' do
+    num = params[:number].to_i
     phrase = params[:phrase]
-    puts phrase.times.num.to_i
+    result = ''
+    num.times do
+      result << phrase
+    end
+    "#{result}"
   end
 
-  get 'say/:word1/:word2/:word3/:word4/:word5' do
-
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end
 
-  get 'say/:operation/:number1/:number2' do
-    
+  get '/:operation/:number1/:number2' do
+    operation = params[:operation]
+    number1 = params[:number1].to_i
+    number2 = params[:number2].to_i
+    if operation == "add"
+      answer = number1 + number2
+    elsif operation == "subtract"
+      answer = number1 - number2
+    elsif operation == "multiply"
+      answer = number1 * number2
+    else operation == "divide"
+      answer = number1 / number2
+    end
+    answer.to_s
   end
 
 end
