6747df7c064885af7fdcedb516ad1a5594675161
Mariel F <marielbeth@gmail.com>
2017-09-09 16:40:40 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,49 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
+
+  get "/reversename/:name" do
+    @name = params[:name].reverse
+    @name
+  end
+
+  get "/square/:number" do
+    @number = params[:number].to_i
+    "#{@number ** 2}"
+  end
+
+  get "/say/:number/:phrase" do
+    @number = params[:number].to_i
+    @phrase = params[:phrase].gsub("%20", " ")
+    final_phrase = ""
+    @number.times {final_phrase += "#{@phrase} "}
+    final_phrase.strip
+  end
+
+  get "/say/:word1/:word2/:word3/:word4/:word5" do
+    @word1 = params[:word1]
+    @word2 = params[:word2]
+    @word3 = params[:word3]
+    @word4 = params[:word4]
+    @word5 = params[:word5]
+    sentence = [@word1, @word2, @word3, @word4, @word5].join(" ")
+    sentence += "."
+  end
+
+  get "/:operation/:number1/:number2" do
+    @operation = params[:operation]
+    @number1 = params[:number1].to_i
+    @number2 = params[:number2].to_i
+    case @operation
+    when "add"
+      "#{@number1 + @number2}"
+    when "subtract"
+      "#{@number1 - @number2}"
+    when "multiply"
+      "#{@number1 * @number2}"
+    when "divide"
+      "#{@number1 / @number2}"
+    end
+  end
 
 end
