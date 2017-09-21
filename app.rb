ec738c56b93ddbbe06b26dcf1f538403c921cbfb
Derek  Pelt <derekpelt@gmail.com>
2017-09-21 13:56:11 +0000

    Done.

--- app.rb
+++ app.rb
@@ -25,18 +25,48 @@ class App < Sinatra::Base
     "#{@test}"
   end
 
-  get '/:operation/:number1/:number2' do
-    @test = params[:splat]
+  #get '/*/*/*' do
+  #  @test = params[:splat]
+  #  @op = @test[0]
+  #  @num1 = @test[1].to_i
+  #  @num2 = @test[2].to_i
+  #  @ops = {"add" => "+", "subtract" => "-", "multiply" => "*", "divide" => "/"}
+  #  @total = @num1.send(@ops[@op], @num2)
+  #  "#{@total}"
+ #end
+
+  get '/add/:number1/:number2' do
     @num1 = params[:number1].to_i
-    @num2 = paramsp[:number2].to_i
-    @test = params
-    #@op = @test[0]
-    #@num1 = @test[1].to_i
-    #@num2 = @test[2].to_i
-    @ops = {"add" => "+", "subtract" => "-", "multiply" => "*", "divide" => "/"}
-    @total = @num1.send(@ops[@op], @num2)
-    "#{@test}"
+    @num2 = params[:number2].to_i
+    @total = @num1 + @num2
+    "#{@total}"
   end
+
+  get '/subtract/:num1/:num2' do
+    @num1 = params[:num1].to_i
+    @num2 = params[:num2].to_i
+    @total = @num1 - @num2
+    "#{@total}"
+  end
+
+  get '/multiply/:num1/:num2' do
+    @num1 = params[:num1].to_i
+    @num2 = params[:num2].to_i
+    @total = @num1 * @num2
+    "#{@total}"
+  end
+
+  get '/divide/:num1/:num2' do
+    @num1 = params[:num1].to_i
+    @num2 = params[:num2].to_i
+    @total = @num1 / @num2
+    "#{@total}"
+  end
+
+
+
+
+
 
 
 
