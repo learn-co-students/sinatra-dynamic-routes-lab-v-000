6df6c1856da7daa3e5c75ce437c76a9e54ccba55
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-10-10 14:53:04 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,33 @@
+require 'pry'
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
+
+  get '/reversename/:name' do
+    @reversed_name =  params[:name].reverse
+    erb :reversedname
+  end
+
+  get '/square/:number' do
+    @num = params[:number].to_i
+    erb :squared_number
+  end
+
+  get '/say/:number/:phrase' do
+    @num = params[:number].to_i
+    @phrase = params[:phrase]
+    erb :repeater
+  end
+  
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    erb :concatenate
+  end
+
+  get '/:operation/:number1/:number2' do
+    @num1 = params[:number1].to_i
+    @num2 = params[:number2].to_i
+    @op = params[:operation]
+    erb :operation
+  end
 
 end
