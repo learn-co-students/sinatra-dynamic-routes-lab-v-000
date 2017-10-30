c12c81d42a9e91fe9bc6eb4c837826f75a7b1b32
github username <github email address>
2017-10-30 11:16:06 -0400

    Done.

--- app.rb
+++ app.rb
@@ -3,45 +3,71 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
   get '/reversename/:name' do
-    @name_reversed = params[:name].reverse!
-    "#{@name_reversed}"
+    params[:name].reverse
+    # @name_reversed = params[:name].reverse
+    # "#{@name_reversed}"
   end
 
   get '/square/:number' do
-    @number = params[:number].to_i
-    "#{@number**2}"
+    (params[:number].to_i ** 2).to_s
+    # @number = params[:number].to_i
+    # "#{@number**2}"
   end
 
   get '/say/:number/:phrase' do
-    @number = params[:number].to_i
-    @phrase = params[:phrase]
-    @phrase*@number
+    answer = ''
+
+    params[:number].to_i.times do
+      answer += params[:phrase]
+    end
+
+    answer
+    # @number = params[:number].to_i
+    # @phrase = params[:phrase]
+    # @phrase*@number
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
     #that accepts five words and returns a string with them formatted as a sentence.
-    @word1 = params[:word1]
-    @word2 = params[:word2]
-    @word3 = params[:word3]
-    @word4 = params[:word4]
-    @word5 = params[:word5]
-    "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
+    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
+
+    # @word1 = params[:word1]
+    # @word2 = params[:word2]
+    # @word3 = params[:word3]
+    # @word4 = params[:word4]
+    # @word5 = params[:word5]
+    # "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
   end
 
   get '/:operation/:number1/:number2' do
-    @operation = params[:operation]
-    @number1 = params[:number1].to_i
-    @number2 = params[:number2].to_i
-    case @operation
-    when "add"
-      "#{@number1+@number2}"
-    when "subtract"
-      "#{@number1-@number2}"
-    when "multiply"
-      "#{@number1*@number2}"
-    when "divide"
-      "#{@number1/@number2}"
+    number1 = params[:number1].to_i
+    number2 = params[:number2].to_i
+
+    answer = 'Unable to perform this operation'
+
+    case params[:operation]
+    when 'add'
+      answer = (number1 + number2).to_s
+    when 'subtract'
+      answer = (number1 - number2).to_s
+    when 'multiply'
+      answer = (number1 * number2).to_s
+    when 'divide'
+      answer = (number1 / number2).to_s
     end
 
+    # @operation = params[:operation]
+    # @number1 = params[:number1].to_i
+    # @number2 = params[:number2].to_i
+    # case @operation
+    # when "add"
+    #   "#{@number1+@number2}"
+    # when "subtract"
+    #   "#{@number1-@number2}"
+    # when "multiply"
+    #   "#{@number1*@number2}"
+    # when "divide"
+    #   "#{@number1/@number2}"
+    # end
   end
 end
