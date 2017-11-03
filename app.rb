e19d94b3efbd8ec95302de99315d57c2f0bb4b88
Nicholas Halloran <nmhalloran@hotmail.com>
2017-11-03 00:46:02 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,43 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+  get '/' do
+    "This is just the boring start page."
+  end
+
+  get '/reversename/:name' do
+    @name = params[:name].reverse
+  end
+
+  get '/square/:number' do
+    @number = params[:number].to_i
+    "#{@number * @number}"
+  end
+
+  # get '/say/:number/:phrase' do
+  #   @number = params[:number].to_i
+  #   @phrase = params[:phrase]
+  #   @number.times do
+  #     @phrase
+  #   end
+  # end
+
+  get 'say/:word1/:word2/:word3/:word4/:word5' do
+    binding.pry
+    @word1 = params[:word1]
+    @word2 = params[:word2]
+    @word3 = params[:word3]
+    @word4 = params[:word4]
+    @word5 = params[:word5]
+    binding.pry
+
+    "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
+  end
+
+  get '/:operation/:number1/:number2' do
+    @operation = params[:operation]
+    @number1 = params[:number1].to_i
+    @number2 = params[:number2].to_i
+  end
 
 end
