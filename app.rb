51768e84c1b5298700320594582f72ad7ff011a4
Julianne Kim <juliannne@gmail.com>
2017-11-14 04:45:57 +0000

    Done.

--- app.rb
+++ app.rb
@@ -3,26 +3,38 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
   get '/reversename/:name' do
-    @user_name = params[:name]
-    "#{@user_name.reverse}"
+    params[:name].reverse
   end
 
   get '/square/:number' do
-    @square = params[:number].to_i * params[:number].to_i
-    "#{@square}"
+    (params[:number].to_i ** 2).to_s
   end
 
   get '/say/:number/:phrase' do
-    @saying = params[:phrase] * params[:number].to_i
-    "#{@saying}"
+    answer = ""
+    params[:number].to_i.times do
+      answer += params[:phrase]
+    end
+    answer
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
-    @sentence = params[:word1] + " " + params[:word2] + " " + params[:word3] + " " + params[:word4] + " " + params[:word5] + "."
-    "#{@sentence}"
+    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end
 
   get '/:operation/:number1/:number2' do
-    erb :math 
+    number1 = params[:number1].to_i
+    number2 = params[:number2].to_i
+
+    case params[:operation]
+    when 'add'
+      answer = (number1 + number2).to_s
+    when 'subtract'
+      answer = (number1 - number2).to_s
+    when 'multiply'
+      answer = (number1 * number2).to_s
+    when 'divide'
+      answer = (number1 / number2).to_s
+    end
   end
 end
