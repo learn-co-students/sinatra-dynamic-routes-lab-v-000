013caffd7c6675deaf3106a18b9c748bbb7b97f3
Miriam Peskowitz <miriam5000@gmail.com>
2017-11-17 18:49:55 +0000

    Done.

--- app.rb
+++ app.rb
@@ -11,6 +11,16 @@ class App < Sinatra::Base
     (params[:number].to_i ** 2).to_s
   end
 
+  get '/say/:number/:phrase' do
+    params[:phrase] * (params[:number].to_i))
+  end
 
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+       "#{params[:word1]} #{params[:word3]} #{params[:word3]} #{params[:word4]} #{params[:word5]} ."
+  end
+
+  get get '/:operation/:number1/:number2' do
+    fparams[:number1].to_i params[:operation] params[:number2].to_i
+  end
 
 end
