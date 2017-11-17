f9e10376b0fe4bfcb25eb5c6d4d0e156eea05725
Miriam Peskowitz <miriam5000@gmail.com>
2017-11-17 19:01:19 +0000

    Done.

--- app.rb
+++ app.rb
@@ -12,15 +12,32 @@ class App < Sinatra::Base
   end
 
   get '/say/:number/:phrase' do
-    params[:phrase] * (params[:number].to_i))
+    params[:phrase] * (params[:number].to_i)
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
-       "#{params[:word1]} #{params[:word3]} #{params[:word3]} #{params[:word4]} #{params[:word5]} ."
+       "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end
 
-  get get '/:operation/:number1/:number2' do
-    fparams[:number1].to_i params[:operation] params[:number2].to_i
-  end
+  get '/:operation/:number1/:number2' do
 
+    first_number = params[:number1].to_i
+    second_number = params[:number2].to_i
+
+    result = ""
+
+    case params[:operation]
+    when 'add'
+      result = (first_number + second_number).to_s
+
+    when 'subtract'
+      result = (first_number - second_number).to_s
+
+    when 'multiply'
+      result = (first_number * second_number).to_s
+
+    when 'divide'
+      result = (first_number / second_number).to_s
+    end
+  end
 end
