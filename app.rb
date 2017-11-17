a66c73d3b55f3485bef83b2a5080d3e0a38668f1
Miriam Peskowitz <miriam5000@gmail.com>
2017-11-17 18:43:46 +0000

    Done.

--- app.rb
+++ app.rb
@@ -3,4 +3,14 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
 
+  get '/reversename/:name' do
+    params[:name].reverse
+  end
+
+  get '/square/:number' do
+    (params[:number].to_i ** 2).to_s
+  end
+
+
+
 end
