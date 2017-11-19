a242e557ec45485b35067fde84761f6f57768165
Chris Roberts <chrisrobertspdx@gmail.com>
2017-11-19 19:07:31 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,43 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+  get "/reversename/:name" do
+    params[:name].reverse
+  end
+
+  get "/square/:num" do
+    "#{params[:num].to_i * params[:num].to_i}"
+  end
+
+  get "/say/:number/:phrase" do
+    @count = params[:number].to_i
+    @final = ""
+    @count.times do
+      @final += params[:phrase] + "\n"
+    end
+    "#{@final}"
+  end
+
+  get "/say/:word1/:word2/:word3/:word4/:word5" do
+    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
+  end
+
+  get "/:operation/:number1/:number2" do
+    @num1 = params[:number1].to_i
+    @num2 = params[:number2].to_i
+    @operation = params[:operation]
+    case @operation
+    when "add"
+      "#{@num1 + @num2}"
+    when "subtract"
+      "#{@num1 - @num2}"
+    when "multiply"
+      "#{@num1 * @num2}"
+    when "divide"
+      "#{@num1 / @num2}"
+    else
+      "Undefined operation."
+    end
+  end
 
 end
