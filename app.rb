1480f907c605ad7ff365c1548bd6f741ae609fc7
github username <github email address>
2017-12-03 17:41:28 +0200

    Done.

--- app.rb
+++ app.rb
@@ -3,4 +3,50 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
 
+  get '/reversename/:name' do
+    @name = params[:name]
+    "#{@name}".reverse
+  end
+
+  get '/square/:number' do
+    @number = params[:number].to_i
+    "#{@number ** 2}"
+  end
+
+  get '/say/:number/:phrase' do
+    @number = params[:number].to_i
+    @phrase = params[:phrase]
+
+    @phrase_n_times = []
+    @number.times do
+       @phrase_n_times << @phrase
+     end
+     @phrase_n_times
+    end
+
+    get '/say/:word1/:word2/:word3/:word4/:word5' do
+      @word1 = params[:word1]
+      @word2 = params[:word2]
+      @word3 = params[:word3]
+      @word4 = params[:word4]
+      @word5 = params[:word5]
+      "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
+    end
+
+    get '/:operation/:number1/:number2' do
+      @operation = params[:operation]
+      @number1 = params[:number1].to_i
+      @number2 = params[:number2].to_i
+
+      case @operation
+        when 'add'
+          @number1.+(@number2).to_s
+        when 'subtract'
+          @number1.-(@number2).to_s
+        when 'multiply'
+          @number1.*(@number2).to_s
+        when 'divide'
+          @number1./(@number2).to_s
+        end
+    end
 end
