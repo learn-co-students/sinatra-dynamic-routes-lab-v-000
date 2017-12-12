46ef18078221db0c38f2a2022e49c29dbbfa3bd6
Michael Evans <michael@michaelgregoryevans.com>
2017-12-12 18:56:20 +0000

    Done.

--- app.rb
+++ app.rb
@@ -30,15 +30,21 @@ class App < Sinatra::Base
     @operation = params[:operation]
     @number1 = params[:number1].to_i
     @number2 = params[:number2].to_i
-    #binding.pry
-    if @operation == 'add'
-      "#{@number1 + @number2}"
-    elsif @operation == 'subtract'
-      "#{@number1 - @number2}"
-    elsif @operation == 'multiply'
-      "#{@number1 * @number2}"
-    else @operation == 'divide'
-      "#{@number1 / @number2}"
-    end
+
+    @operation_symbol = '+' if @operation == 'add'
+    @operation_symbol = '-' if @operation == 'subtract'
+    @operation_symbol = '*' if @operation == 'multiply'
+    @operation_symbol = '/' if @operation == 'divide'
+
+    "#{@number1.send(@operation_symbol, @number2)}"
+    #if @operation == 'add'
+      #"#{@number1 + @number2}"
+    #elsif @operation == 'subtract'
+      #"#{@number1 - @number2}"
+    #elsif @operation == 'multiply'
+      #"#{@number1 * @number2}"
+    #else @operation == 'divide'
+      #"#{@number1 / @number2}"
+    #end
   end
 end
