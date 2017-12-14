e97af0377d572eaee15cb23fc36f275d62910c48
EC2 Default User <ec2-user@ip-172-31-38-157.us-east-2.compute.internal>
2017-12-14 20:57:59 +0000

    app.rb

--- app.rb
+++ app.rb
@@ -1,6 +1,42 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
-
+  
+    get '/reversename/:name' do
+        @name = params[:name].reverse
+    end
+    
+    get '/square/:number' do
+        @number = params[:number].to_i
+        "#{@number * @number}"
+    end
+    
+    get '/say/:number/:phrase' do 
+        @number = params[:number].to_i
+        @phrase = params[:phrase]
+        @phrase_array = []
+        @number.times do 
+            @phrase_array << @phrase 
+        end
+        @phrase_array.join
+    end
+    
+    get '/say/:word1/:word2/:word3/:word4/:word5' do 
+        "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
+    end
+    
+    get '/:operation/:number1/:number2' do 
+        @operation = params[:operation]
+        @number1 = params[:number1].to_i
+        @number2 = params[:number2].to_i
+        if @operation == "add"
+            "#{@number1 + @number2}"
+        elsif @operation == "subtract"
+            "#{@number1 - @number2}"
+        elsif @operation == "multiply"
+            "#{@number1 * @number2}"
+        else 
+            "#{@number1/@number2}"
+        end
+    end
 end
