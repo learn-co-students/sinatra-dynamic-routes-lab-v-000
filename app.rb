cdfda74855aa8061d0f89279a149506761aee98e
github username <github email address>
2017-12-30 18:08:40 -0500

    Done.

--- app.rb
+++ app.rb
@@ -22,8 +22,7 @@ class App < Sinatra::Base
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
-    @result = "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
-    "#{@result}"
+    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end
 
   get '/:operation/:number1/:number2' do
