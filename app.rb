da72b247b45196f17bbd21b92180198466d6c9e8
Ilya Zarembsky <wlycdgrfromflatiron@gmail.com>
2018-02-08 15:19:42 -0500

    Rspeclemented get '/square/:number'

--- app.rb
+++ app.rb
@@ -5,4 +5,9 @@ class App < Sinatra::Base
   get '/reversename/:name' do
     "#{params[:name].reverse}"
   end
+
+  get '/square/:number' do
+    @square = (params[:number].to_i)**2
+    "#{@square.to_s}"
+  end
 end
