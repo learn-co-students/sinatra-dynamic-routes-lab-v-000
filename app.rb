a7dfc6293279b8d66ddf588478a7670dd8ffd21c
Denis Atkesone <denis.atkesone@gmail.com>
2018-02-09 14:48:26 +0000

    Done.

--- app.rb
+++ app.rb
@@ -3,4 +3,40 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
 
+  get '/reversename/:name' do
+    erb :reversename
+  end
+
+  get '/square/:number' do
+    erb :square
+  end
+
+  get '/say/:number/:phrase' do
+    erb :say
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    erb :say
+  end
+
+  get '/:operation/:number1/:number2' do
+
+    n1, n2 = params[:number1].to_i, params[:number2].to_i
+
+    case params[:operation]
+    when "add"
+      total = n1 + n2
+    when "subtract"
+      total = n1 - n2
+    when "multiply"
+      total = n1 * n2
+    when "divide"
+      total = n1 / n2
+    else
+      "Not a valid operation"
+    end
+
+    total.to_s
+  end
+
 end
