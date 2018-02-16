6f2f85f23e409cb18769fd7f09be1e902fade5fe
Blakeley Bond <burrick2003@yahoo.com>
2018-02-16 18:54:21 +0000

    Done.

--- app.rb
+++ app.rb
@@ -3,22 +3,29 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
   get '/reversename/:name' do
-
+      params[:name].reverse
   end
 
   get '/square/:number' do
-
+      @answer = params[:number].to_i ** 2
+      "#{@answer.to_s}"
   end
 
-  get '/say/:number/:phrase'
-
+  get '/say/:number/:phrase' do
+      temp = params[:phrase]
+      counter = 1
+        while counter < params[:number].to_i
+          temp = temp + params[:phrase]
+          counter += 1
+        end
+      temp
   end
 
-  get '/say/:word1/:word2/:word3/:word4/:word5'
-
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+      "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}." #doh, period
   end
 
-  get '/:operation/:number1/:number2'
+  get '/:operation/:number1/:number2' do
       if params[:operation] == "add"
         @answer = params[:number1].to_i + params[:number2].to_i
         "#{@answer.to_s}"
@@ -33,5 +40,4 @@ class App < Sinatra::Base
         "#{@answer.to_s}"
       end
   end
-
 end
