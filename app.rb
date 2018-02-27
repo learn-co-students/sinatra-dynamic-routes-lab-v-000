15c677eb3c72c19c596a6a3d7c8fd423445727d9
Jamie Giuliano <jamiepgiuliano@gmail.com>
2018-02-26 16:42:56 -0800

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,36 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
+  get '/' do
+    "Dynamic Routes Lab"
+  end
+
+  get '/reversename/:name' do
+
+    erb :reversename
+  end
+
+  get '/square/:number' do
+    @number = params[:number].to_i
+
+    erb :square
+  end
+
+  get '/say/:number/:phrase' do
+
+    erb :saynumberphrase
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+
+    erb :saysentence
+  end
+
+  get '/:operation/:number1/:number2' do
+    @num1 = params[:number1].to_i
+    @num2 = params[:number2].to_i
+
+    erb :operation
+  end
 
 end
