2257881138f7162d023884ec51473fea7542d13b
Katrina Brinson <kbrinso@gmail.com>
2018-02-28 00:12:49 +0000

    Starting square

--- app.rb
+++ app.rb
@@ -2,5 +2,15 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+  get '/reversename/:name' do
+     @user_name = params[:name].reverse
+  #   #binding.pry
+  # "#{@username}"
+    erb :reversename
+  end
 
+  get '/square/:number' do
+    @num = params[:number]
+    "#{@num*@num}"
+  end
 end
