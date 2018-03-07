8c7a11dab5b95ee7dd954338b3aaec3e8c362223
suzy baldwin <gbaldwin24@comcast.net>
2018-03-07 17:17:22 +0000

    Done.

--- app.rb
+++ app.rb
@@ -3,4 +3,16 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
 
+
+  get "/reversename/:name" do
+    @user_name = params[:name]
+
+    "#{@user_name.reverse}"
+  end
+get "/square/:number" do
+
+  x = params[:number.to_i]
+
+
+end
 end
