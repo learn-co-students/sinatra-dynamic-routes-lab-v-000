155bfd9da8197983660451cac8fc898d817e636f
Robert Hughes <roberts.ginger.email@gmail.com>
2018-03-11 17:03:17 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,39 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
+  get "/reversename/:name" do
+    @reversed = params[:name].reverse
+    "#{@reversed}"
+  end
 
+  get "/square/:number" do
+    @squared = params[:number].to_i ** 2
+    "#{@squared}"
+  end
+
+  get "/say/:number/:phrase" do
+    @phrases = params[:phrase] * params[:number].to_i
+    "#{@phrases}"
+  end
+
+  get "/say/:word1/:word2/:word3/:word4/:word5" do
+    "#{params.values.join(" ")}."
+  end
+
+  get "/:operation/:number1/:number2" do
+    n1 = params[:number1].to_i
+    n2 = params[:number2].to_i
+    op = params[:operation]
+    if op == "add"
+      "#{n1 + n2}"
+    elsif op == "subtract"
+      "#{n1 - n2}"
+    elsif op == "multiply"
+      "#{n1 * n2}"
+    elsif op == "divide"
+      "#{n1 / n2}"
+    else
+      "#{op} is not a valid operator"
+    end
+  end
 end
