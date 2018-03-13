c58e0ab8c1a54a368b1aaede0fa35492fe55e786
YanC Lin <yonalin1@gmail.com>
2018-03-13 03:56:56 +0000

    Done.

--- app.rb
+++ app.rb
@@ -21,9 +21,21 @@ class App < Sinatra::Base
     s
   end
 
-  # get '/say/:word1/:word2/:word3/:word4/:word5' do
-  #     params[:word]
-  # end
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+   @w1 = params[:word1]
+   @w2 = params[:word2]
+   @w3 = params[:word3]
+   @w4 = params[:word4]
+   @w5 = params[:word5]
+   erb :saywords
+  end
+
+  get '/:operation/:number1/:number2' do
+    @operation = params[:operation]
+    @num1 = params[:number1].to_i
+    @num2 = params[:number2].to_i
+    erb :operation
+  end
 
 
 end
