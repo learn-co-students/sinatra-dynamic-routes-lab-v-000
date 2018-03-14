fe14e1d59c6b7d6feb5e4795af81761e94034ab9
Nicole Eidi <nicoleeidi1@gmail.com>
2018-03-14 17:09:53 +0000

    Done.

--- app.rb
+++ app.rb
@@ -10,13 +10,16 @@ get '/square/:number' do
   "#{@number ** 2}"
 end
 get '/say/:number/:phrase' do
-  @number= params[:number]
-  binding.pry
-  i=0
-  until i= @number
-    "#{params[:phrase]}"
-    i+=1
+  @number= params[:number].to_i
+  #binding.pry
+  #i=0
+  #until i= @number
+  string= ""
+  @number.times do
+    string += params[:phrase]
+  # i+=1
   end
+  string
 end
 get '/say/:word1/:word2/:word3/:word4/:word5' do
   # @word1= params[:word1]
@@ -25,11 +28,20 @@ get '/say/:word1/:word2/:word3/:word4/:word5' do
   # @word4= params[:word4]
   # @word5= params[:word5]
 
-  "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}}"
+  "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}.}"
 end
 get '/:operation/:number1/:number2' do
-  result= params[:number1].to_i + params[:operation] + params[:number2].to_i
-  "#{result}"
-
+  #binding.pry
+    if params[:operation] == "add"
+      result= params[:number1].to_i + params[:number2].to_i
+    elsif params[:operation] == "subtract"
+      result= params[:number1].to_i - params[:number2].to_i
+    elsif params[:operation] == "multiply"
+      result= params[:number1].to_i * params[:number2].to_i
+    else
+      result= params[:number1].to_i/params[:number2].to_i
+    end
+  #  binding.pry
+    result.to_s
 end
 end
