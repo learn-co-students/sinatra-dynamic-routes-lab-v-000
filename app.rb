7e0c7aa79a827562703739ca6567cf212a336b8f
Eden Mesfun <emesfun@gmail.com>
2018-03-21 19:34:58 +0000

    Done.

--- app.rb
+++ app.rb
@@ -20,14 +20,34 @@ class App < Sinatra::Base
   get '/say/:number/:phrase' do
     @phrase = params[:phrase]
     @n = params[:number].to_i
-    counter = 0
 
-    loop do
-      "#{@phrase}"
-      counter += 1
-      break if counter == @n
+    @return = @phrase * @n
+    @return
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    @join = [params[:word1],params[:word2], params[:word3], params[:word4], params[:word5]].join(" ")
+    "#{@join}."
+  end
+
+  get '/:operation/:number1/:number2' do
+    @num1 = params[:number1].to_i
+    @num2 = params[:number2].to_i
+    @operation = params[:operation]
+
+    if @operation == "add"
+      @add = @num1 + @num2
+      "#{@add}"
+    elsif @operation == "subtract"
+      @subtract = @num2 - @num1
+      "#{@subtract}"
+    elsif @operation == "multiply"
+      @multiply = @num1 * @num2
+      "#{@multiply}"
+    else @operation == "divide"
+      @divide = @num1 / @num2
+      "#{@divide}"
     end
-
-  end #get say
+  end
 
 end
