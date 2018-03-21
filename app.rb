42fb233e21d892b0984bd3263d4f341c772dafcb
Eden Mesfun <emesfun@gmail.com>
2018-03-21 18:53:28 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,33 @@
+require 'pry'
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
+
+  get '/' do
+    "This is my root page"
+  end
+
+  get '/reversename/:name' do
+    @reverse = params[:name].reverse
+    "#{@reverse}"
+  end
+
+  get '/square/:number' do
+    @square = (params[:number].to_i)**2
+    "#{@square}"
+  end
+
+  get '/say/:number/:phrase' do
+    @phrase = params[:phrase]
+    @n = params[:number].to_i
+    counter = 0
+
+    loop do
+      "#{@phrase}"
+      counter += 1
+      break if counter == @n
+    end
+
+  end #get say
 
 end
