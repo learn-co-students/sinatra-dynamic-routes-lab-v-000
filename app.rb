055903c3b63eb1d5c8f0d37bdbc201316b97b23f
Joshua Vickstrom <joshuavickstrom@outlook.com>
2018-03-28 19:28:05 +0000

    Done.

--- app.rb
+++ app.rb
@@ -16,9 +16,7 @@ class App < Sinatra::Base
   get '/say/:number/:phrase' do
     num = params[:number].to_i
     phrase = params[:phrase]
-    num.times do |phrase|
-      puts phrase
-    end
+    return phrase * num
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
