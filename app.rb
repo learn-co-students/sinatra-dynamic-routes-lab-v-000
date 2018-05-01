b3b4a470a17204b6d73a50156aaba04d5da55b9c
Joanne Lee <vernapii@gmail.com>
2018-05-01 15:37:40 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,18 @@
 require_relative 'config/environment'
-
+require 'pry'
 class App < Sinatra::Base
   # Write your code here!
+  get '/reversename/:name' do
+    binding.pry
+  end
 
+  get '/square/:number' do
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+
+  end
+
+  get '/:operation/:number1/:number2' do
+  end
 end
