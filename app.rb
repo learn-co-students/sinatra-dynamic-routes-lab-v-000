3fc2a46c395ade13b3733568c1ac9a55c71aab4d
Emily Zhao <tong.zhao.92@gmail.com>
2018-05-07 22:22:44 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,20 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+  get "/reversename/:name" do
+    "My name in reverse is #{params[:name].reverse}"
+  end
+
+  get "/square/:number" do
+    a = params[:number].to_i
+    "The square result is #{a**2}"
+  end
+
+  get "/say/:number/:phrase" do
+    n = params[:number].to_i
+    n.times do
+      puts "#{params[:phrase]}"
+    end
+  end
 
 end
