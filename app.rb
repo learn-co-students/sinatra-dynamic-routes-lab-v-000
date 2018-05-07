622ee857027663cec1c8495fc0c41c13425385f3
Trevor Seitz <trevinla@gmail.com>
2018-05-07 17:52:11 +0000

    Done.

--- app.rb
+++ app.rb
@@ -12,19 +12,36 @@ class App < Sinatra::Base
     @square.to_s
   end
 
-  get 'say/:number/:phrase' do
-    num = params[:number].to_f
-    num.times {
-      puts params[:phrase]
-    }
+  get '/say/:number/:phrase' do
+    @phrase = ""
+    num = params[:number].to_i
+    num.times do
+      @phrase += params[:phrase]
+    end
+    "#{@phrase}"
   end
 
-  get 'say/:word1/:word2/:word3/:word4/:word5' do
-
-
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end
 
   get '/:operation/:number1/:number2' do
+    num1 = params[:number1].to_f
+    num2 = params[:number2].to_f
 
+    case params[:operation]
+    when "add"
+      sum = (num1 + num2).to_s
+      sum
+    when "subtract"
+      dif = (num1 - num2).to_s
+      dif
+    when "multiply"
+      prod = (num1 * num2).to_s
+      prod
+    when "divide"
+      quo = (num1 / num2).to_s
+      quo
+    end
   end
 end
