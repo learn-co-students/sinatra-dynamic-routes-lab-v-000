928c489c9f0947bfd320ff6a5be6d14e08e9ca70
Raiet Intraub <raietsaeed@gmail.com>
2018-05-10 00:59:20 +0000

    Done.

--- app.rb
+++ app.rb
@@ -23,13 +23,26 @@ class App < Sinatra::Base
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
-    @user_name = params[:name]
-    "Hello #{@user_name}!"
+    @string = params[:word1] + params[:word2] + params[:word3] + params[:word4] + params[:word5]
+    "#{@string}"
   end
 
   get '/:operation/:number1/:number2' do
-    @user_name = params[:name]
-    "Hello #{@user_name}!"
+    @number1 = params[:number1].to_i
+    @number2 = params[:number2].to_i
+    @operation = params[:operation]
+    case @operation
+    when "add"
+      @number1 + @number2
+    when "subtract"
+      @number1 - @number2
+    when "multiply"
+      @number1 * @number2
+    when "divide"
+      @number1 / @number2
+    else
+      "Select an applicable operation : add, subtract, multiply, or divide"
+    end
   end
 
 end
