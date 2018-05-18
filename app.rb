c11a002e5e4e49394b6fc4d587bc361ce0cd56e0
Kriti Rai <kriti.rai@gmail.com>
2018-05-18 00:37:36 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,27 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
 
+  get '/reversename/:name' do
+    @reversed_name = params[:name].reverse
+    "#{@reversed_name}"
+  end
+
+  get '/square/:number' do
+    @square = (params[:number].to_i * params[:number].to_i).to_s
+    "#{@square}"
+  end
+
+  get '/say/:number/:phrase' do
+
+    erb :phrase
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    "#{params[:word1]}" + " #{params[:word2]}" +" #{params[:word3]}" +" #{params[:word4]}" +" #{params[:word5]}" + "."
+  end
+
+  get '/:operation/:number1/:number2' do
+    erb :operation
+  end
 end
