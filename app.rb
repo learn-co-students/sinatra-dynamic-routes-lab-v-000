b3243b8502e4ea8034d651790c2cc2a71959347d
David Markel <davidgmarkel@gmail.com>
2018-06-01 17:56:11 +0000

    Done.

--- app.rb
+++ app.rb
@@ -22,14 +22,15 @@ class App < Sinatra::Base
   end
 
   get "/:operation/:number1/:number2" do
-    if params[:operation] == "add"
-      "#{params[:number1].to_i + params[:number2].to_i}"
-    elsif params[:operation] == "subtract"
-      "#{params[:number1].to_i - params[:number2].to_i}"
-    elsif params[:operation] == "multiply"
-      "#{params[:number1].to_i * params[:number2].to_i}"
-    elsif params[:operation] == "divide"
-      "#{params[:number1].to_i / params[:number2].to_i}"
+    case params[:operation]
+      when "add"
+        "#{params[:number1].to_i + params[:number2].to_i}"
+      when "subtract"
+        "#{params[:number1].to_i - params[:number2].to_i}"
+      when "multiply"
+        "#{params[:number1].to_i * params[:number2].to_i}"
+      when "divide"
+        "#{params[:number1].to_i / params[:number2].to_i}"
     end
   end
 
