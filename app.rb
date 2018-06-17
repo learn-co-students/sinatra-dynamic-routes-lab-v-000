9e5a64639cdb45aab810f2b1e2005743d4a155e8
ho-tonym <ho.tonym@gmail.com>
2018-06-16 20:27:37 -0400

    Done.

--- app.rb
+++ app.rb
@@ -3,20 +3,20 @@ require 'pry'
 
 class App < Sinatra::Base
   get '/reversename/:name' do
-    @name = params[:name]
-    "#{@name.reverse}"
+    name = params[:name]
+    "#{name.reverse}"
   end
 
   get '/square/:number' do
-    @number = params[:number].to_i
+    number = params[:number].to_i
     #binding.pry
-    "#{@number * @number}"
+    "#{number * number}"
   end
 
   get '/say/:number/:phrase' do
-    @times = params[:number].to_i
-    @phrase = params[:phrase]
-    @phrase * @times
+    times = params[:number].to_i
+    phrase = params[:phrase]
+    phrase * times
     #binding.pry
   end
 
