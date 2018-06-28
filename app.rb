47f49eca6ae14dfc579dec238fb7aef0b44257e9
Jeremiah Lutes <jeremiahlutes@gmail.com>
2018-06-28 16:22:10 +0000

    Done.

--- app.rb
+++ app.rb
@@ -21,8 +21,7 @@ class App < Sinatra::Base
   end
 
   get "/say/:word1/:word2/:word3/:word4/:word5" do
-    @words = params[:word1] + " " + params[:word2] + " " +  params[:word3] + " " + params[:word4] + " " + params[:word5] +"."
-    "#{@words}"
+   "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end
 
   get "/:operation/:number1/:number2" do
