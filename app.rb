83232a8382b18004ae61d524aff955e9c1bc8981
Ron Sala <ron.sala@gmail.com>
2018-06-29 10:54:09 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,5 +1,4 @@
 require_relative 'config/environment'
-require 'pry'
 
 class App < Sinatra::Base
 
@@ -22,13 +21,21 @@ class App < Sinatra::Base
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
-    @reversed = params[:name].reverse
-    "#{@reversed}"
+    params[:word1] + ' ' + params[:word2] + ' ' + params[:word3] + ' ' +  params[:word4] + ' ' + params[:word5] + '.'
   end
 
   get '/:operation/:number1/:number2' do
-    @reversed = params[:name].reverse
-    "#{@reversed}"
+    case params[:operation]
+    when 'add'
+      result = params[:number1].to_i + params[:number2].to_i
+    when 'subtract'
+      result = params[:number1].to_i - params[:number2].to_i
+    when 'multiply'
+      result = params[:number1].to_i * params[:number2].to_i
+    when 'divide'
+      result = params[:number1].to_i / params[:number2].to_i
+    end
+    result.to_s
   end
 
 end
