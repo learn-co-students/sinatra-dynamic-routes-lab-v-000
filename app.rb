b5dec9d29fc28617cb3fc96c1b84a412b5c28bd2
Eric DeSimone <eric.desimone@cortland.edu>
2018-07-03 11:08:02 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,10 +1,44 @@
 require_relative 'config/environment'
+require 'pry'
 
 class App < Sinatra::Base
 
   get '/reversename/:name' do
     @name = params[:name]
     "#{@name.reverse}"
+  end
+
+  get '/square/:number' do
+    @result = params[:number].to_i**2
+    "#{@result}"
+  end
+
+  get '/say/:number/:phrase' do
+    @phrase = params[:phrase]
+    @number = params[:number].to_i
+    @total_phrase = []
+    @number.times {@total_phrase << @phrase}
+    @total_phrase
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    @phrase = "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
+    @phrase
+  end
+
+  get '/:operation/:number1/:number2' do
+    @operation = params[:operation]
+    @number1 = params[:number1].to_i
+    @number2 = params[:number2].to_i
+    if @operation == "add"
+      "#{@number1 + @number2}"
+    elsif @operation == "subtract"
+      "#{@number2 - @number1}"
+    elsif @operation == "multiply"
+      "#{@number1 * @number2}"
+    elsif @operation == "divide"
+      "#{@number1 / @number2}"
+    end
   end
 
 end
