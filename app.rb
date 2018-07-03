c1d8aad9f6e967fb5e420ecb539d75bd1e30e01a
Eric DeSimone <eric.desimone@cortland.edu>
2018-07-03 10:35:03 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,10 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
+
+  get '/reversename/:name' do
+    @name = params[:name]
+    "#{@name.reverse}"
+  end
 
 end
