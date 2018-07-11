c0470b13a59916017256daf4c95fa96e917e4611
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-07-11 21:01:24 +0000

    Done.

--- app.rb
+++ app.rb
@@ -14,8 +14,25 @@ class App < Sinatra::Base
   end
 
   get "/say/:number/:phrase" do
-    params[:number].times do |x|  #params[:number] must be integer
+    params[:number].to_i.times.collect do |x|
       params[:phrase]
+    end
+  end
+
+  get "/say/:word1/:word2/:word3/:word4/:word5" do
+    "#{params.values.join(" ")}."
+  end
+
+  get "/:operation/:number1/:number2" do
+    result = ""
+    if params[:operation] == "add"
+      (params[:number1].to_i + params[:number2].to_i).to_s
+    elsif params[:operation] == "subtract"
+      (params[:number1].to_i - params[:number2].to_i).to_s
+    elsif params[:operation] == "multiply"
+      (params[:number1].to_i * params[:number2].to_i).to_s
+    else
+      (params[:number1].to_i / params[:number2].to_i).to_s
     end
   end
 
