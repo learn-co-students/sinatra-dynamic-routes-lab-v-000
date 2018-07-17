7d4046d256933d13b2d18a76f9a7dce4b33d7b68
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-07-17 19:23:43 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require_relative 'config/environment'
+require "pry"
 
 class App < Sinatra::Base
   # Write your code here!
@@ -7,29 +8,50 @@ class App < Sinatra::Base
       "#{@user_name}"
     end
 
-    get "/hello/:name" do
-    @user_name = params[:name]
-    "Hello #{@user_name}!"
-  end
-
   get "/square/:number" do
-    @user_name = params[:name]
-    "Hello #{@user_name}!"
+    @number = params[:number].to_i ** 2
+    "#{@number}"
   end
 
   get "/say/:number/:phrase" do
-    @user_name = params[:name]
-    "Hello #{@user_name}!"
+    answer = ''
+    @phrase = params[:phrase]
+    @number = params[:number].to_i
+    @number.times do |x|
+     answer += @phrase
+    end
+    answer
   end
 
   get "/say/:word1/:word2/:word3/:word4/:word5" do
-    @user_name = params[:name]
-    "Hello #{@user_name}!"
+    answer = ''
+    @word1 = params[:word1]
+    @word2 = params[:word2]
+    @word3 = params[:word3]
+    @word4 = params[:word4]
+    @word5 = params[:word5]
+    answer += @word1 + " "
+    answer += @word2 + " "
+    answer += @word3 + " "
+    answer += @word4 + " "
+    answer += @word5
+    answer += "."
+    answer
   end
 
   get "/:operation/:number1/:number2" do
-    @user_name = params[:name]
-    "Hello #{@user_name}!"
+    num1 = params[:number1].to_i
+    num2 = params[:number2].to_i
+    answer = "Dont know that function"
+    case params[:operation]
+    when "add"
+      answer = (num1 + num2).to_s
+    when "subtract"
+      answer = (num1 - num2).to_s
+    when "divide"
+      answer = (num1 / num2).to_s
+    when "multiply"
+      answer = (num1 * num2).to_s
+    end
   end
-
 end
