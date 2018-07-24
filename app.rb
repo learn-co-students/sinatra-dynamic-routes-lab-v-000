87c091ce01579718915795c2f2c79e5ca10d7fb5
Scott Weinstein <sweinstein27@gmail.com>
2018-07-24 17:45:45 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
+
+  get '/reversename/:name' do
+    @reversename = params[:name].reverse
+    "#{@reversename}"
+  end
+
+  get 'square/:number' do
+    @square = params[:number].to_i.square!.to_s
+    "#{@square}"
+  end
 
 end
