7b90a4ef288302128b3d84fc7d347932e047b27c
David McGrann <dpmcgrann@gmail.com>
2018-08-11 18:30:54 +0000

    Done.

--- app.rb
+++ app.rb
@@ -42,6 +42,24 @@ class App < Sinatra::Base
     end
   end
   
+  #alternate solution
   
+  # get '/:operation/:number1/:number2' do
+  #   number1 = params[:number1].to_i
+  #   number2 = params[:number2].to_i
+
+  #   answer = 'Unable to perform this operation'
+
+  #   case params[:operation]
+  #   when 'add'
+  #     answer = (number1 + number2).to_s
+  #   when 'subtract'
+  #     answer = (number1 - number2).to_s
+  #   when 'multiply'
+  #     answer = (number1 * number2).to_s
+  #   when 'divide'
+  #     answer = (number1 / number2).to_s
+  #   end
+  # end
   
 end
