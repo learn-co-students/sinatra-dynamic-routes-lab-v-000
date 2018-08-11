0ed44f1799ff2ef93de4062e6fc2373a9a438b7a
David McGrann <dpmcgrann@gmail.com>
2018-08-11 18:29:47 +0000

    Done.

--- app.rb
+++ app.rb
@@ -4,29 +4,23 @@ class App < Sinatra::Base
   # Write your code here!
   
   get "/reversename/:name" do
-    @name = params[:name]
-    "#{@name.reverse}"
+    params[:name].reverse
   end
   
   get '/square/:number' do
-    @square = params[:number].to_i ** 2 
-    "#{@square}"
+    (params[:number].to_i ** 2).to_s 
   end
   
   get '/say/:number/:phrase' do
-    @num = params[:number].to_i
-    @phrase = params[:phrase]
-    @phrase * @num
+    phrase = ''
+    params[:number].to_i.times do
+      phrase += params[:phrase]
+    end
+    phrase
   end
   
   get '/say/:word1/:word2/:word3/:word4/:word5' do
-    @w1 = params[:word1]
-    @w2 = params[:word2]
-    @w3 = params[:word3]
-    @w4 = params[:word4]
-    @w5 = params[:word5]
-    @s = "#{@w1} #{@w2} #{@w3} #{@w4} #{@w5}"
-    "#{@s}."
+    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end
   
   get '/:operation/:number1/:number2' do
