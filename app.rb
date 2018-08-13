e3596ff8be1fd334b89787b329ea4db1c5944148
Olena Ageyeva <eageeva@hotmail.com>
2018-08-13 13:29:24 +0000

    Done.

--- app.rb
+++ app.rb
@@ -18,13 +18,32 @@ class App < Sinatra::Base
   end
 
 get '/say/:number/:phrase' do
-  @n=params[:number]
-  @phrase=params[:phrase]
-  @final_phrase=""
-  for i in 1..@n do
-    @final_phrase=@final_phrase+@phrase
+  n=params[:number].to_i
+  phrase=params[:phrase]
+  final_phrase=""
+  for i in 1..n do
+    final_phrase+="#{phrase}\n"
   end
-  @final_phrase
+  final_phrase
 end
 
+get '/say/:word1/:word2/:word3/:word4/:word5' do
+ "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
+end
+
+get '/:operation/:number1/:number2' do
+  if params[:operation] == "add"
+       answer = params[:number1].to_i + params[:number2].to_i
+     elsif params[:operation] == "subtract"
+       answer = params[:number1].to_i - params[:number2].to_i
+     elsif params[:operation] == "multiply"
+       answer = params[:number1].to_i * params[:number2].to_i
+     elsif params[:operation] == "divide"
+       answer = params[:number1].to_i / params[:number2].to_i
+     else
+       answer = "Unknown"
+     end
+     answer.to_s
+
+end
 end
