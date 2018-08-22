f10591b84e89c53c3c242c306190e06330b81705
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-08-22 00:35:16 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,46 @@
 require_relative 'config/environment'
+require 'pry'
 
 class App < Sinatra::Base
   # Write your code here!
+   get '/reversename/:name' do
+      params[:name].reverse
+   end
+
+   get '/square/:number' do
+      "#{params[:number].to_i**2}"
+   end
+
+   get '/say/:number/:phrase' do
+      # binding.pry
+      # @x = params[:number].to_i
+      # @y = params[:phrase]
+      params[:number].to_i.times {|i| puts params[:phrase]}
+      # @x.times {|i| puts @y}
+      # binding.pry
+   end
+
+   get '/say/:word1/:word2/:word3/:word4/:word5' do
+      @x = []
+      @y = params.each {|key, value| @x << value}
+      @x.join(' ')+'.'
+   end
+
+   get '/:operation/:number1/:number2' do
+      @x = params[:operation]
+      @y = params[:number1].to_i
+      @z = params[:number2].to_i
+      if @x == "add"
+         "#{@y + @z}"
+      elsif @x == "subtract"
+         "#{@y - @z}"
+      elsif @x == "multiply"
+         "#{@y * @z}"
+      elsif @x == "divide"
+         "#{@y / @z}"
+      end
+   end
 
 end
+
+
--- config.ru
+++ config.ru
@@ -1,3 +1,3 @@
 require './config/environment'
-
+# require 'sinatra'
 run App
