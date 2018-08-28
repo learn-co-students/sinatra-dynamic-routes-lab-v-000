5134ffde7eed236518208e02f6dab6e5f238a682
github username <github email address>
2018-08-28 14:13:00 +0200

    Done.

--- app.rb
+++ app.rb
@@ -3,4 +3,72 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
 
+  get '/reversename/:name' do 
+    @name = params[:name].reverse
+    "#{@name}"
+  end
+
+
+  get '/square/:number' do 
+    @number = params[:number].to_i
+    @number = params[:number].to_i
+    "#{@number * @number}"
+  end
+
+
+  # get '/say/:number/:phrase' do
+  #   @number = params[:number].to_i
+  #   @prase = params[:prhrase]
+  #   "#{@phrase * @number}"
+  # end
+
+  get '/say/:number/:phrase' do
+    @number = params[:number].to_i
+    @phrase = params[:phrase]
+    erb :say
+  end
+
+  # get '/say/:word1/:word2/:word3/:word4/:word5' do
+  #   @w1 = params[:word1]
+  #   @w2 = params[:word2]
+  #   @w3 = params[:word3]
+  #   @w4 = params[:word4]
+  #   @w5 = params[:word5]
+  #   erb :saywords
+
+
+  # end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    @w1 = params[:word1]
+    @w2 = params[:word2]
+    @w3 = params[:word3]
+    @w4 = params[:word4]
+    @w5 = params[:word5]
+    erb :saywords
+  end
+
+  # get '/:operation/:number1/:number2' do
+  #   @operation = params[:opertation]
+  #   @num1 = params[:number1].to_i
+  #   @num2 = params[:number2].to_i
+  #   erb :operation
+
+
+
+  # end
+
+  get '/:operation/:number1/:number2' do
+    @operation = params[:operation]
+    @num1 = params[:number1].to_i
+    @num2 = params[:number2].to_i
+    erb :operation
+  end
+
+
+
+
+
+
+
 end
