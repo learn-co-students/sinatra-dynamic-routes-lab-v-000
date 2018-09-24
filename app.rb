e52f7fa31ad78f30f63f96db9570d3f061df378c
Christia T <christiatidwell@bellsouth.net>
2018-09-24 18:24:05 +0000

    created reverse name route

--- app.rb
+++ app.rb
@@ -3,4 +3,9 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
 
+  get '/reversename/:name' do
+    @user_name = params[:name].reverse
+    "#{@user_name}"
+  end
+
 end
