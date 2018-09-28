a3f5a3ed4abbb73a02c8d3c6b0bb04103dc236ec
Leslie Stevens <flylady@mail.utexas.edu>
2018-09-28 02:04:56 +0000

    Done.

--- app.rb
+++ app.rb
@@ -31,6 +31,22 @@ class App < Sinatra::Base
       #binding.pry
       @word1 + " " + @word2 + " " + @word3 + " " + @word4 + " " + @word5 + "."
     end
+
+    get "/:operation/:number1/:number2" do
+      operation = params[:operation]
+      num1 = params[:number1].to_i
+      num2 = params[:number2].to_i
+      #binding.pry
+      if operation == "add"
+        num1 + num2
+      elsif operation == "subtract"
+        num1 - num2
+      elsif operation == "multiply"
+        num1 * num2
+      elsif operation == "divide"
+        num1/num2
+      end
+    end
       # Write your code here!
 
 end
