94d9a103e4c592a86323eaf89851aa982635ac98
Adam Barton <adam.barton333@gmail.com>
2018-10-11 22:18:21 +0000

    Done.

--- app.rb
+++ app.rb
@@ -22,11 +22,25 @@ class App < Sinatra::Base
       end
 
     get '/say/:word1/:word2/:word3/:word4/:word5' do
-      "#{word1} #{word2} #{word3} #{word4} #{word5}."
+
+      "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
     end
 
     get '/:operation/:number1/:number2' do
-      "Hello World"
+      operation = params[:operation]
+      num1 = params[:number1].to_i
+      num2 = params[:number2].to_i
+
+        if operation == "add"
+          content = num1 + num2
+        elsif operation == "subtract"
+          content = num1 - num2
+        elsif operation == "multiply"
+          content = num1 * num2
+        elsif operation == "divide"
+          content = num1 / num2
+        end
+        content.to_s
     end
 
 end
