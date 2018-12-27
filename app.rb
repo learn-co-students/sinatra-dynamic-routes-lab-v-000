361a1640ff0aa3ca128f7f60c9b6f3e1b7612f7d
Rafael Lopez <lpz.raf@gmail.com>
2018-12-27 18:33:48 +0000

    Done.

--- app.rb
+++ app.rb
@@ -11,12 +11,17 @@ class App < Sinatra::Base
     "#{@num}"
  end
  
-  get '/say/:number/:phrase' do
+ get '/say/:number/:phrase' do
     n = params[:number].to_i
     s = ""
 
     n.times {s << params[:phrase]}
     s
+  end
+  
+  get "/say/:word1/:word2/:word3/:word4/:word5" do
+   @w = params[:word1] + " " + params[:word2] + " " + params[:word3] + " " + params[:word4] + " " + params[:word5] + "."  
+   "#{@w}"
   end
   
 
