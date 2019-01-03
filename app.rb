728313358f1d9337c41c207c301ccf0bf734afa9
Sharmaine Requerme <srqrme@gmail.com>
2019-01-03 18:46:11 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,13 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+  get 'reversename/:name' do
+    @reversename = params[:name].reverse
+    "#{@reversename}"
+  end
 
+  get '/multiply/:num1/:num2' do
+    @multiplied = params[:num1].to_i * params[:num2].to_i
+    "#{@multiplied}"
+  end
 end
