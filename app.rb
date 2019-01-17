29c37abbeaed2c6a73bb6d454f9f3baf2a0c5f46
Heidi Hlavinka <hchlav@gmail.com>
2019-01-17 12:10:36 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
+  get "/reversename/:name" do
+    @user_name = params[:name]
+    @user_name.reverse
+  end
 
+  get "/square/:number" do
+    @number = params[:number]
+    # try a ** b
+    # or Integer.sqrt(2)
+  end
 end
+
+
+# binding.pry
