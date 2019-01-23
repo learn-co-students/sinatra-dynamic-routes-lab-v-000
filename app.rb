464b9201988398282e826536b7584b76732e29be
Heidi Hlavinka <hchlav@gmail.com>
2019-01-23 01:48:10 +0000

    Done.

--- app.rb
+++ app.rb
@@ -25,9 +25,18 @@ class App < Sinatra::Base
   end
 
   get '/:operation/:number1/:number2' do
-    "#{params[:number1]} params[:operation] #{params[:number2]}"
-    binding.pry
-    #convert to integers and do if/else statement?
+    num1 = params[:number1].to_i
+    num2 = params[:number2].to_i
+
+    if params[:operation] == "add"
+      "#{num1 + num2}"
+    elsif params[:operation] == "subtract"
+      "#{num1 - num2}"
+    elsif params[:operation] == "multiply"
+      "#{num1 * num2}"
+    else params[:operation] == "divide"
+      "#{num1/num2}"
+    end
   end
 
 
