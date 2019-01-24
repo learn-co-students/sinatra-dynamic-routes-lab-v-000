f6ca74b133c59d64904c16008508aa4f25ddb66b
Yancy Zhang <yuanxi.zhang@gmail.com>
2019-01-24 01:17:32 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,42 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
+  get '/reversename/:name' do 
+    @new = params[:name].split().reverse.join
+    erb :reversename
+  end
 
+  get '/square/:number' do
+    @square = (params[:number].to_i) ** 2 
+    erb :square 
+  end
+  
+  get '/say/:number/:phrase' do 
+    @words = params[:phase] * params[:number]
+    erb :say 
+  end
+  
+  get '/say/:word1/:word2/:word3/:word4/:word5' do 
+    @word_list = params[:word1] + params[:word2] + params[:word3] + params[:word4] + params[:word5]
+    erb :say
+  end
+    
+  get '/:operation/:number1/:number2' do 
+    if params[:operation] == "+"
+      @answer = (params[:number1].to_i) + (params[:number2].to_i)
+    elsif params[:operation] == "-"
+      @answer = (params[:number1].to_i) - (params[:number2].to_i)
+    elsif params[:operation] == "*"
+      @answer = (params[:number1].to_i) * (params[:number2].to_i)
+    elsif params[:operation] == "/"
+      @answer = (params[:number1].to_i) / (params[:number2].to_i)
+    elsif params[:operation] == "**"
+      @answer = (params[:number1].to_i) ** (params[:number2].to_i)
+    elsif params[:operation] == "%"
+      @answer = (params[:number1].to_i) % (params[:number2].to_i)  
+    else 
+      @answer = "The operation can not be completed."
+    end
+    erb :index 
+  end
 end
