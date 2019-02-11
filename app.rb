a1c854d896b8688c4000d1f18fd4ccc997f32be9
nanton83 <nicholas.romans@gmail.com>
2019-02-11 15:05:05 -0500

    Done.

--- app.rb
+++ app.rb
@@ -4,3 +4,20 @@ class App < Sinatra::Base
   # Write your code here!
 
 end
+
+# Instructions
+# Create a dynamic route at get '/reversename/:name' that accepts a name and renders the name backwards.
+
+# Create a dynamic route at get '/square/:number' 
+# that accepts a number and returns the square of that number. Note: Remember that values in params 
+# always come in as strings, and your return value for the route should also be a string (use .to_i and .to_s).
+
+# Create a dynamic route at get '/say/:number/:phrase' that accepts a number and a phrase and returns that 
+# phrase in a string the number of times given.
+
+# Create a dynamic route at get '/say/:word1/:word2/:word3/:word4/:word5' that accepts five words and 
+# returns a string containing all five words (i.e. word1 word2 word3 word4 word5).
+
+# Create a dynamic route at get '/:operation/:number1/:number2' that accepts an operation 
+# (add, subtract, multiply or divide) and performs the operation on the two numbers provided. 
+# For example, going to /add/1/2 should render 3.
