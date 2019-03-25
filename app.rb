811744d24162030aee952d3a0241a7a20c5aeff5
Sheena Bonilla <sheena.myb@gmail.com>
2019-03-25 01:14:22 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,30 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
 
+  #accepts the name and renders name backwards
+  get '/reversename/:name' do
+    @user_name = params[:name].reverse
+    "#{@user_name}"
+  end
+
+  get '/square/:number' do
+    @number = params[:number].to_i
+    "#{@number*@number}"
+  end
+
+  get '/say/:number/:phrase' do
+    @phrase = params[:phrase].gsub(' ', '%20')
+    @number = params[:number].to_i
+    @number.times {"#{@phrase}"}
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    @word1 = params[:word1]
+    @word2 = params[:word2]
+    @word3 = params[:word3]
+    @word4 = params[:word4]
+    @word5 = params[:word5]
+    "#{@word1}" + " #{@word2}" + " #{@word3}" + " #{@word4}" + " #{@word5}" + "."
+  end
 end
