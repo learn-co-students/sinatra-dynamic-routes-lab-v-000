7be50f6946ede5b92be84048eca558818331b33c
Mariel Hoepelman <marielhoepelman@gmail.com>
2019-04-09 15:49:21 +0000

    Done.

--- app.rb
+++ app.rb
@@ -4,6 +4,7 @@ class App < Sinatra::Base
   # Write your code here!
   get "/reversename/:name" do
     @reversed_name = params[:name].reverse
+    "#{@reversed_name}"
   end
 
   get "/square/:number" do
