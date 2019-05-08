8fdfb3c3c8aaa5fbb183b83940869500368bdf9e
Reno Sorensen <renojames19@gmail.com>
2019-05-08 04:44:13 +0000

    Done.

--- app.rb
+++ app.rb
@@ -18,6 +18,17 @@ class App < Sinatra::Base
       @repeats << @phrase
     end
     "#{@repeats.join(" ")}"
+    #There HAS to be a better way to do this... For some reason I kept getting a lint error when I tried
+    #to print using an iterator.
+  end
+
+  get "/say/:word1/:word2/:word3/:word4/:word5" do
+    @word1 = params[:word1]
+    @word2 = params[:word2]
+    @word3 = params[:word3]
+    @word4 = params[:word4]
+    @word5 = params[:word5]
+    "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
   end
 
 end
