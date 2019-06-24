7b89e64a48989458c27845af6d306816e763a44b
Steven Crouse <sdcrouse91@gmail.com>
2019-06-24 08:15:39 +0000

    Done.

--- app.rb
+++ app.rb
@@ -17,7 +17,23 @@ class App < Sinatra::Base
   end
   
   get '/say/:word1/:word2/:word3/:word4/:word5' do 
+    params.values.join(" ") + "."
+  end
+  
+  get '/:operation/:number1/:number2' do 
+    @number1 = params[:number1].to_i
+    @number2 = params[:number2].to_i
     
+    case params[:operation]
+    when "add"
+      (@number1 + @number2).to_s
+    when "subtract"
+      (@number1 - @number2).to_s
+    when "multiply"
+      (@number1 * @number2).to_s
+    when "divide"
+      (@number1 / @number2).to_s
+    end
   end
 
 end
