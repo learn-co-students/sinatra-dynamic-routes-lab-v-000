d1f08dca1b738c7110eb9e0980d3a94b777d5fd7
Karly Mareka <karly.mareka@gmail.com>
2019-06-27 22:38:51 +0000

    Done.

--- app.rb
+++ app.rb
@@ -22,6 +22,11 @@ class App < Sinatra::Base
     @phrases 
   end
   
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    @five_words = String.new 
+    @five_words = params[:word1] + " " + params[:word2] + " " + params[:word3] + " " + params[:word4] + " " + params[:word5] + "." 
+  end
+  
   
   
 end 
