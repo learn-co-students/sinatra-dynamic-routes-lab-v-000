6aeab801eb7de238ce7bf678d3b30591a87acf1c
Chao Hsuan Chi <allen0413@gmail.com>
2019-08-28 07:04:04 +0000

    Done.

--- app.rb
+++ app.rb
@@ -3,44 +3,41 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
   get "/reversename/:name" do
-    @name = params[:name].reverse
-    "Goodbye, #{@name}."
+    params[:name].reverse
   end
 
   get "/square/:number" do
-    @num = params[:number].to_i
-    @squared = @num*@num
-    @fin = @squared.to_s
-    "#{@fin}"
+    (params[:number].to_i ** 2).to_s
   end
 
   get "/say/:number/:phrase" do
-    @num = params[:number].to_i
-    @phrase = params[:phrase]
-    @num.times do
-      @fin += @phrase
+    fin = ""
+
+    params[:number].to_i.times do
+      fin += params[:phrase]
     end
-    @fin
+
+    fin
   end
 
   get "/say/:word1/:word2/:word3/:word4/:word5" do
-    @w1 = params[:word1]
-    @w2 = params[:word2]
-    @w3 = params[:word3]
-    @w4 = params[:word4]
-    @w5 = params[:word5]
-    @fin = []
-    @fin << @w1
-    @fin << @w2
-    @fin << @w3
-    @fin << @w4
-    @fin << @w5
-    "#{@fin}"
+    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end
 
   get "/:operation/:number1/:number2" do
-    @num1 = params[:number1].to_i
-    @num2 = params[:number2].to_i
-    "Goodbye, #{@name}."
+    num1 = params[:number1].to_i
+    num2 = params[:number2].to_i
+
+    case params[:operation]
+    when 'add'
+      answer = (num1 + num2).to_s
+    when 'subtract'
+      answer = (num1 - num2).to_s
+    when 'multiply'
+      answer = (num1 * num2).to_s
+    when 'divide'
+      answer = (num1 / num2).to_s
+    end
   end
+
 end
