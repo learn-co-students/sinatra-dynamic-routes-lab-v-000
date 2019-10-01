be71da893c14721274994c7decc2e169a76ac6b7
Karen Flores <karen.ag.flores@gmail.com>
2019-10-01 22:34:01 +0000

    Done.

--- app.rb
+++ app.rb
@@ -15,13 +15,9 @@ class App < Sinatra::Base
   end
 
   get '/say/:number/:phrase' do
-    @number = params[:number]
+    @number = params[:number].to_i
     @phrase = params[:phrase]
-    @i = @number.to_i
-    while @i >= 1
-      puts "#{@phrase}"
-      @i = @i -1
-    end
+    "#{@phrase * @number}"
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
@@ -34,7 +30,21 @@ class App < Sinatra::Base
   end
 
   get '/:operation/:number1/:number2' do
+    @operation = params[:operation]
+    @number1 = params[:number1].to_i
+    @number2 = params[:number2].to_i
 
+    if @operation == 'add'
+      "#{@number1 + @number2}"
+    elsif @operation == 'subtract'
+      "#{@number1 - @number2}"
+    elsif @operation == 'multiply'
+      "#{@number1 * @number2}"
+    elsif @operation == 'divide'
+      "#{@number1/@number2}"
+    else
+      "Sorry. Not applicable"
+    end
   end
 
 end
