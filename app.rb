4b7ce43505052ac25ff12d9ca2925fadac3c1f00
Jonathan Fernandez <jfern20@gmail.com>
2020-01-07 05:10:18 +0000

    Done.

--- app.rb
+++ app.rb
@@ -10,7 +10,39 @@ class App < Sinatra::Base
      "#{params[:number].to_i ** 2}"
   end 
   
-  
-  
+  get '/say/:number/:phrase' do 
+   answer = ''
+   params[:number].to_i.times do 
+    answer += params[:phrase]
+   #accepts a number and a phrase
+   #and returns that phrase in a single string the number of times given.
+ end 
+  answer 
+end 
 
-end
+ get '/say/:word1/:word2/:word3/:word4/:word5' do 
+   "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
+   end 
+   
+   get '/:operation/:number1/:number2' do 
+     number1 = params[:number1].to_i
+     number2 = params[:number2].to_i 
+     
+     answer = "unable to perform this operation"
+     
+     case params[:operation]
+     when 'add'
+        answer = (number1 + number2).to_s 
+        
+       when 'subtract'
+         answer = (number1 - number2).to_s 
+         
+         when 'multiply'
+           answer = (number1 * number2).to_s 
+           
+           when 'divide'
+             answer = (number1 / number2).to_s 
+            
+          end 
+         end
+       end
