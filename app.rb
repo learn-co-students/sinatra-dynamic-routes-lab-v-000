16ae033e054b388af68f9b9078cdc289de7c3ff9
ortufte <ortufte@gmail.com>
2020-01-23 17:16:11 -0600

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,48 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
+
+  get '/reversename/:name' do
+    @name = params[:name]
+    @name.reverse 
+  end
+
+  get '/square/:number' do
+    @number = params[:number]
+    square = @number.to_i * @number.to_i
+    square.to_s
+  end
+
+  get '/say/:number/:phrase' do
+    @number = params[:number]
+    @phrase = params[:phrase]
+    n = @number.to_i
+      @phrase * n
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    @word1 = params[:word1]
+    @word2 = params[:word2]
+    @word3 = params[:word3]
+    @word4 = params[:word4]
+    @word5 = params[:word5]
+    "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
+  end
+
+  get '/:operation/:number1/:number2' do
+    @operation = params[:operation]
+    @number1 = params[:number1]
+    @number2 = params[:number2]
+ 
+    if @operation == "add"
+      (@number1.to_i + @number2.to_i).to_s
+    elsif @operation == "subtract"
+      (@number1.to_i - @number2.to_i).to_s
+    elsif @operation == "divide"
+      (@number1.to_i/@number2.to_i).to_s
+    elsif @operation == "multiply"
+      (@number1.to_i * @number2.to_i).to_s
+    end
+  end
 
 end
