2b5d5457f934974d65ffe4e219de068fa531b222
Luis Herrera <herrera.lrh@gmail.com>
2020-04-15 00:19:15 +0000

    Done.

--- app.rb
+++ app.rb
@@ -3,20 +3,20 @@ require 'pry'
 
 class App < Sinatra::Base
 
-  get "/reversename/:name" do
+  get '/reversename/:name' do
     @name = params[:name]
     @name.reverse
   end
 
-  get "/square/:number" do
+  get '/square/:number' do
     @number = params[:number].to_i
     @number_squared = @number ** 2
     @number_squared.to_s
   end
 
-  get "/say/:number/:phrase" do
-    @phrase = []
+  get '/say/:number/:phrase' do
     @number = params[:number].to_i
+    @phrase = []
 
     @number.times do
       @phrase << params[:phrase]
@@ -24,26 +24,23 @@ class App < Sinatra::Base
     @phrase
   end
 
-  get "/say/:word1/:word2/:word3/:word4/:word5" do
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
     "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end
 
-  get "/:operation/:number1/:number2" do
-    @number1 = params[:number1].to_i
-    @number2 = params[:number2].to_i
+  get '/:operation/:num1/:num2' do
+    @num1 = params[:num1].to_i
+   @num2 = params[:num2].to_i
 
-    case params[:operation]
-    when 'add'
-      @answer = (@number1 + @number2).to_s
-    when 'subtract'
-      @answer = (@number1 - @number2).to_s
-    when 'multiply'
-      @answer = (@number1 * @number2).to_s
-    when 'divide'
-      @answer = (@number1 / @number2).to_s
-    end
+   case params[:operation]
+   when 'add'
+     @answer = (@num1 + @num2).to_s
+   when 'subtract'
+     @answer = (@num1 - @num2).to_s
+   when 'multiply'
+     @answer = (@num1 * @num2).to_s
+   when 'divide'
+     @answer = (@num1 / @num2).to_s
+   end
   end
-
-
-
 end
