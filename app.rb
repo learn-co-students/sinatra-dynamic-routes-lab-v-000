68b34d8c21cf7c59dc3f96bcf0ad938e9ee62f69
Daniel Suarez <suarez.daniel101@gmail.com>
2021-05-21 21:34:29 -0400

    Done.

--- app.rb
+++ app.rb
@@ -19,6 +19,12 @@ class App < Sinatra::Base
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
-    
+    @word1 = params[:word1]
+    @word2 = params[:word2]
+    @word3 = params[:word3]
+    @word4 = params[:word4]
+    @word5 = params[:word5]
+    "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
   end
+
 end
