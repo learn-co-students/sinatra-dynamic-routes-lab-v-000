fcdc6aba470030c156317616ad3c4849901c4c62
Daniel Suarez <suarez.daniel101@gmail.com>
2021-05-21 20:47:23 -0400

    Done.

--- app.rb
+++ app.rb
@@ -12,4 +12,10 @@ class App < Sinatra::Base
     "#{@number.to_i ** 2}"
   end
   
+  get '/say/:number/:phrase' do
+    @number = params[:number]
+    @phrase = params[:phrase]
+    "#{@phrase * @number.to_i}"
+  end
+
 end
